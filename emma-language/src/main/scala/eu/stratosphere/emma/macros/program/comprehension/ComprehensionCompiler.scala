78f4efcb862bda0c56116fbd42619ff29798b99a
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2014-10-02 15:56:42 +0200

    Initial stub for comprehension expansion.

--- emma-language/src/main/scala/eu/stratosphere/emma/macros/program/comprehension/ComprehensionCompiler.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/macros/program/comprehension/ComprehensionCompiler.scala
@@ -1,19 +1,31 @@
 package eu.stratosphere.emma.macros.program.comprehension
 
-import eu.stratosphere.emma.macros.program.util.ProgramUtils
-
-import scala.collection.mutable
 import eu.stratosphere.emma.macros.program.ContextHolder
 import eu.stratosphere.emma.macros.program.controlflow.ControlFlowModel
+import eu.stratosphere.emma.macros.program.util.ProgramUtils
 
 import scala.reflect.macros._
 
 private[emma] trait ComprehensionCompiler[C <: blackbox.Context]
   extends ContextHolder[C]
+  with ControlFlowModel[C]
   with ComprehensionModel[C]
   with ProgramUtils[C] {
 
   import c.universe._
+
+  /**
+   * Expands a comprehended term in the compiled driver.
+   *
+   * @param tree The original program tree.
+   * @param cfGraph The control flow graph representation of the tree.
+   * @param comprehensionStore A store containing the comprehended terms in the tree.
+   * @param t The comprehended term to be expanded.
+   * @return A tree representing the expanded comprehension.
+   */
+  def expand(tree: Tree, cfGraph: CFGraph, comprehensionStore: ComprehensionStore)(t: ComprehendedTerm) = {
+    t.term
+  }
 
   /**
    * Serializes a macro-level IR tree as code constructing an equivalent runtime-level IR tree.
