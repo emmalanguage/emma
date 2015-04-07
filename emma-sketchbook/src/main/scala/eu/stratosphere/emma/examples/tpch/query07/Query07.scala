b3c6d9c810f8ad32126f00550ab98a2d3724e44e
Andreas Salzmann <redshark_123@web.de>
2015-04-07 14:53:00 +0200

    [EXAMPLES] changed SQL query in comment

--- emma-sketchbook/src/main/scala/eu/stratosphere/emma/examples/tpch/query07/Query07.scala
+++ emma-sketchbook/src/main/scala/eu/stratosphere/emma/examples/tpch/query07/Query07.scala
@@ -62,14 +62,42 @@ object Query07 {
  *
  * {{{
  * select
- *    sum(l_extendedprice*l_discount) as revenue
- * from
- *    lineitem
- *  where
- *    l_shipdate >= date '[DATE]'
- *    and l_shipdate < date '[DATE]' + interval '1' year
- *    and l_discount between [DISCOUNT] - 0.01 and [DISCOUNT] + 0.01
- *    and l_quantity < [QUANTITY];
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year, sum(volume) as revenue
+ * from (
+ *    select
+ *      n1.n_name as supp_nation,
+ *      n2.n_name as cust_nation,
+ *      extract(year from l_shipdate) as l_year,
+ *      l_extendedprice * (1 - l_discount) as volume
+ *    from
+ *      supplier,
+ *      lineitem,
+ *      orders,
+ *      customer,
+ *      nation n1,
+ *      nation n2
+ *    where
+ *      s_suppkey = l_suppkey
+ *      and o_orderkey = l_orderkey
+ *      and c_custkey = o_custkey
+ *      and s_nationkey = n1.n_nationkey
+ *      and c_nationkey = n2.n_nationkey
+ *      and (
+ *        (n1.n_name = '[NATION1]' and n2.n_name = '[NATION2]')
+ *        or (n1.n_name = '[NATION2]' and n2.n_name = '[NATION1]')
+ *      )
+ *      and l_shipdate between date '1995-01-01' and date '1996-12-31'
+ * ) as shipping
+ * group by
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year
+ * order by
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year;
  * }}}
  *
  * @param inPath Base input path
