c5788915d2f6091c46bd1aa39428b16ee95be713
Andreas Salzmann <redshark_123@web.de>
2015-04-15 13:25:10 +0200

    [EXAMPLES] Added TPC-H Queries 6-9 with tests.

---
+++ emma-examples/src/main/scala/eu/stratosphere/emma/examples/tpch/Query07.scala
@@ -0,0 +1,173 @@
+package eu.stratosphere.emma.examples.tpch
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import eu.stratosphere.emma.api._
+import eu.stratosphere.emma.examples.Algorithm
+import eu.stratosphere.emma.runtime.Engine
+import net.sourceforge.argparse4j.inf.{Namespace, Subparser}
+
+/**
+ * Original query:
+ *
+ * {{{
+ * select
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year, sum(volume) as revenue
+ * from (
+ *    select
+ *      n1.n_name as supp_nation,
+ *      n2.n_name as cust_nation,
+ *      extract(year from l_shipdate) as l_year,
+ *      l_extendedprice * (1 - l_discount) as volume
+ *    from
+ *      supplier,
+ *      lineitem,
+ *      orders,
+ *      customer,
+ *      nation n1,
+ *      nation n2
+ *    where
+ *      s_suppkey = l_suppkey
+ *      and o_orderkey = l_orderkey
+ *      and c_custkey = o_custkey
+ *      and s_nationkey = n1.n_nationkey
+ *      and c_nationkey = n2.n_nationkey
+ *      and (
+ *        (n1.n_name = '[NATION1]' and n2.n_name = '[NATION2]')
+ *        or (n1.n_name = '[NATION2]' and n2.n_name = '[NATION1]')
+ *      )
+ *      and l_shipdate between date '1995-01-01' and date '1996-12-31'
+ * ) as shipping
+ * group by
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year
+ * order by
+ *    supp_nation,
+ *    cust_nation,
+ *    l_year;
+ * }}}
+ *
+ * @param inPath Base input path
+ * @param outPath Output path
+ * @param nation1 Query parameter `NATION1`
+ * @param nation2 Query parameter `NATION2`
+ */
+class Query07(inPath: String, outPath: String, nation1: String, nation2: String, rt: Engine, val truncate: Boolean = false) extends Algorithm(rt) {
+
+  import eu.stratosphere.emma.examples.tpch.Query07.Schema._
+
+  def this(ns: Namespace, rt: Engine) = this(
+    ns.get[String](Query07.Command.KEY_INPUT),
+    ns.get[String](Query07.Command.KEY_OUTPUT),
+    ns.get[String](Query07.Command.KEY_NATION1),
+    ns.get[String](Query07.Command.KEY_NATION2),
+    rt)
+
+  def run() = {
+
+    val alg = emma.parallelize {
+
+      // cannot directly reference the parameter
+      val _truncate = truncate
+      val _nation1 = nation1
+      val _nation2 = nation2
+
+      val df = new SimpleDateFormat("yyyy-MM-dd")
+      val calendar = Calendar.getInstance()
+
+
+
+      val subquery = for (
+        s <- read(s"$inPath/supplier.tbl", new CSVInputFormat[Supplier]('|'));
+        l <- read(s"$inPath/lineitem.tbl", new CSVInputFormat[Lineitem]('|')); if s.suppKey == l.suppKey; if l.shipDate >= "1995-01-01"; if l.shipDate <= "1996-12-31";
+        o <- read(s"$inPath/orders.tbl", new CSVInputFormat[Order]('|')); if o.orderKey == l.orderKey;
+        c <- read(s"$inPath/customer.tbl", new CSVInputFormat[Customer]('|')); if c.custKey == o.custKey;
+        n1 <- read(s"$inPath/nation.tbl", new CSVInputFormat[Nation]('|')); if s.nationKey == n1.nationKey;
+        n2 <- read(s"$inPath/nation.tbl", new CSVInputFormat[Nation]('|')); if c.nationKey == n2.nationKey;
+        if ((n1.name == _nation1 && n2.name == _nation2) || (n1.name == _nation2 && n2.name == _nation1))
+      )
+      yield {
+        calendar.setTime(df.parse(l.shipDate))
+        new Subquery(n1.name, n2.name, calendar.get(Calendar.YEAR), l.extendedPrice * (1 - l.discount))
+      }
+      // aggregate and compute the final result
+
+      val rslt = for (
+        g <- subquery.groupBy(x => new GrpKey(x.suppNation, x.custNation, x.year)))
+      yield {
+        def tr(v: Double) = if (_truncate) BigDecimal(v).setScale(4, BigDecimal.RoundingMode.HALF_UP).toDouble else v
+
+        new Result(
+          g.key.suppNation,
+          g.key.custNation,
+          g.key.year,
+          tr(g.values.map(x => x.volume).sum()))
+      }
+
+      // write out the result
+      write(outPath, new CSVOutputFormat[Result]('|'))(rslt)
+    }
+
+    alg.run(rt)
+  }
+}
+
+object Query07 {
+
+  object Command {
+    // argument names
+    val KEY_INPUT = "input"
+    val KEY_OUTPUT = "output"
+    val KEY_NATION1 = "nation1"
+    val KEY_NATION2 = "nation2"
+  }
+
+  class Command extends Algorithm.Command[Query07] {
+
+    // algorithm names
+    override def name = "tpch-q7"
+
+    override def description = "TPC-H Query-7"
+
+    override def setup(parser: Subparser) = {
+      // basic setup
+      super.setup(parser)
+
+      // add arguments
+      parser.addArgument(Command.KEY_INPUT)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_INPUT)
+        .metavar("INPATH")
+        .help("base input file")
+      parser.addArgument(Command.KEY_OUTPUT)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_OUTPUT)
+        .metavar("OUTPUT")
+        .help("output file")
+      parser.addArgument(Command.KEY_NATION1)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_NATION1)
+        .metavar("NATION1")
+        .help("nation1")
+      parser.addArgument(Command.KEY_NATION2)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_NATION2)
+        .metavar("NATION2")
+        .help("nation2")
+    }
+  }
+
+  object Schema {
+
+    case class GrpKey(suppNation: String, custNation: String, year: Integer) {}
+
+    case class Subquery(suppNation: String, custNation: String, year: Integer, volume: Double) {}
+
+    case class Result(suppNation: String, custNation: String, year: Integer, revenue: Double) {}
+  }
+}
+
---
+++ emma-examples/src/main/scala/eu/stratosphere/emma/examples/tpch/Query09.scala
@@ -0,0 +1,157 @@
+package eu.stratosphere.emma.examples.tpch
+
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import eu.stratosphere.emma.api._
+import eu.stratosphere.emma.examples.Algorithm
+import eu.stratosphere.emma.runtime.Engine
+import net.sourceforge.argparse4j.inf.{Namespace, Subparser}
+
+/**
+ * Original query:
+ *
+ * {{{
+ * select
+ *    nation,
+ *    o_year,
+ *    sum(amount) as sum_profit
+ * from (
+ *    select
+ *      n_name as nation,
+ *      extract(year from o_orderdate) as o_year,
+ *      l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount
+ *    from
+ *      part,
+ *      supplier,
+ *      lineitem,
+ *      partsupp,
+ *      orders,
+ *      nation
+ *    where
+ *      s_suppkey = l_suppkey
+ *      and ps_suppkey = l_suppkey
+ *      and ps_partkey = l_partkey
+ *      and p_partkey = l_partkey
+ *      and o_orderkey = l_orderkey
+ *      and s_nationkey = n_nationkey
+ *      and p_name like '%[COLOR]%'
+ * ) as profit
+ * group by
+ *    nation,
+ *    o_year
+ * order by
+ *    nation,
+ *    o_year desc;
+ * }}}
+ *
+ * @param inPath Base input path
+ * @param outPath Output path
+ * @param color Query parameter `COLR`
+ */
+class Query09(inPath: String, outPath: String, color: String, rt: Engine, val truncate: Boolean = false) extends Algorithm(rt) {
+
+  import eu.stratosphere.emma.examples.tpch.Query09.Schema._
+
+  def this(ns: Namespace, rt: Engine) = this(
+    ns.get[String](Query09.Command.KEY_INPUT),
+    ns.get[String](Query09.Command.KEY_OUTPUT),
+    ns.get[String](Query09.Command.KEY_COLOR),
+    rt)
+
+  def run() = {
+
+    val alg = emma.parallelize {
+
+      // cannot directly reference the parameter
+      val _truncate = truncate
+      val _color = color
+
+      val df = new SimpleDateFormat("yyyy-MM-dd")
+      val calendar = Calendar.getInstance()
+
+      val subquery = for (
+        p <- read(s"$inPath/part.tbl", new CSVInputFormat[Part]('|'));  if p.name.indexOf(_color) != -1;
+        s <- read(s"$inPath/supplier.tbl", new CSVInputFormat[Supplier]('|'));
+        l <- read(s"$inPath/lineitem.tbl", new CSVInputFormat[Lineitem]('|')); if s.suppKey == l.suppKey; if p.partKey == l.partKey;
+        ps <- read(s"$inPath/partsupp.tbl", new CSVInputFormat[PartSupp]('|')); if ps.suppKey == l.suppKey; if ps.partKey == l.partKey;
+        o <- read(s"$inPath/orders.tbl", new CSVInputFormat[Order]('|')); if o.orderKey == l.orderKey;
+        n <- read(s"$inPath/nation.tbl", new CSVInputFormat[Nation]('|')); if s.nationKey == n.nationKey
+      )
+      yield {
+        calendar.setTime(df.parse(o.orderDate))
+        new Subquery(n.name, calendar.get(Calendar.YEAR), amount = l.extendedPrice * (1 - l.discount) - ps.supplyCost * l.quantity)
+      }
+
+      // aggregate and compute the final result
+      val rslt = for (
+        g <- subquery.groupBy(x => new GrpKey(x.nation, x.year)))
+      yield {
+        def tr(v: Double) = if (_truncate) BigDecimal(v).setScale(4, BigDecimal.RoundingMode.HALF_UP).toDouble else v
+
+        new Result(
+          g.key.nation,
+          g.key.year,
+          tr(g.values.map(x => x.amount).sum())
+        )
+      }
+
+      // write out the result
+      write(outPath, new CSVOutputFormat[Result]('|'))(rslt)
+    }
+
+    alg.run(rt)
+  }
+}
+
+object Query09 {
+
+  object Command {
+    // argument names
+    val KEY_INPUT = "input"
+    val KEY_OUTPUT = "output"
+    val KEY_COLOR = "color"
+  }
+
+  class Command extends Algorithm.Command[Query09] {
+
+    // algorithm names
+    override def name = "tpch-q9"
+
+    override def description = "TPC-H Query-9"
+
+    override def setup(parser: Subparser) = {
+      // basic setup
+      super.setup(parser)
+
+      // add arguments
+      parser.addArgument(Command.KEY_INPUT)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_INPUT)
+        .metavar("INPATH")
+        .help("base input file")
+      parser.addArgument(Command.KEY_OUTPUT)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_OUTPUT)
+        .metavar("OUTPUT")
+        .help("output file")
+      parser.addArgument(Command.KEY_COLOR)
+        .`type`[String](classOf[String])
+        .dest(Command.KEY_COLOR)
+        .metavar("COLOR")
+        .help("color")
+    }
+  }
+
+  object Schema {
+
+    case class Subquery(nation: String, year: Integer, amount: Double) {}
+
+    case class GrpKey(nation:String, year: Integer) {}
+
+    case class Result(nation: String, year: Integer, sumProfit: Double) {}
+
+  }
+
+}
+
