46fc50de5a68abcf4b00dea2c7e5fbd32e64a1a2
Georgi Krastev <joro.kr.21@gmail.com>
2015-04-28 15:06:52 +0200

    Improve the semantic consistency of stateful update operators

--- emma-common/src/main/scala/eu/stratosphere/emma/api/Stateful.scala
+++ emma-common/src/main/scala/eu/stratosphere/emma/api/Stateful.scala
@@ -31,11 +31,12 @@ object Stateful {
      *
      * @param f the update function to be applied for each point in the distributed state.
      */
+    @deprecated("Use updateWith instead.", "28.04.2015")
     def update(f: A => Option[A]): DataBag[A] = DataBag(for (
       s <- state.values.toList;
       d <- f(s)) yield {
-      state(d.identity) = d; d
-    })
+        state(d.identity) = d; d
+      })
 
     /**
      * Computes and applies a delta with one additional input.
@@ -43,12 +44,45 @@ object Stateful {
      * @param f the update function to be applied for each point with matching update in the distributed state.
      * @tparam B A set of update messages to be passed as first extra agument to the update function.
      */
+    @deprecated("Use updateWith instead.", "28.04.2015")
     def update[B <: Identity[K]](updates: DataBag[B])(f: (A, B) => Option[A]): DataBag[A] = DataBag(for (
       u <- updates.impl;
       s <- state.get(u.identity);
       d <- f(s, u)) yield {
-      state(d.identity) = d; d
-    })
+        state(d.identity) = d; d
+      })
+
+    /**
+     * Computes and flattens a delta without additional inputs. The UDF `f` is allowed to modify the state element `s`,
+     * however with the restriction that the modification should not affect it's identity.
+     *
+     * @param f the update function to be applied for each point in the distributed state.
+     * @tparam B The type of the output elements.
+     * @return The flattened result of all update invocations.
+     */
+    def updateWith[B](f: A => DataBag[B]): DataBag[B] =
+      for {
+        s <- bag()
+        d <- f(s)
+      } yield d
+
+    /**
+     * Computes and flattens the `leftJoin` with `updates` which passes for each update element `u` it's
+     * corresponding state element `s` to the update function `f`. The UDF `f` is allowed to modify the current
+     * state element `s`, however with the restriction that the modification should not affect it's identity.
+     *
+     * @param updates A collection of inputs to be joined with.
+     * @param f A UDF to be applied per pair `f(s, u)` as described above.
+     * @tparam B The type of the `updates` elements.
+     * @tparam C The type of the output elements.
+     * @return The flattened result of all update invocations.
+     */
+    def updateWith[B <: Identity[K], C](updates: DataBag[B])(f: (A, B) => DataBag[C]): DataBag[C] =
+      for {
+        u <- updates
+        s <- DataBag(state.get(u.identity).toSeq)
+        d <- f(s, u)
+      } yield d
 
     /**
      * Computes and flattens the `nestJoin(p, f)` which nests the current state elements `s` against their
@@ -76,12 +110,11 @@ object Stateful {
      * @tparam C The type of the output elements.
      * @return The flattened collection
      */
-    def updateWith[B, C](updates: DataBag[B])(k: B => K, f: (A, DataBag[B]) => DataBag[C]): DataBag[C] = {
-      // compute the nestJoin
-      val nestJoin = for (s <- this.bag()) yield f(s, for (u <- updates; if k(u) == s.identity) yield u)
-      // flatten the nestJoin result
-      nestJoin.flatMap[C](x => x)
-    }
+    def updateWith[B, C](updates: DataBag[B])(k: B => K, f: (A, DataBag[B]) => DataBag[C]): DataBag[C] =
+      for {
+        s <- bag()
+        d <- f(s, for (u <- updates; if k(u) == s.identity) yield u)
+      } yield d
 
     /**
      * Converts the stateful bag into an immutable Bag.
