829b636f6f0922f6d25cbb586f521bf8b99d9216
Georgi Krastev <joro.kr.21@gmail.com>
2015-05-25 18:34:05 +0200

    [EXAMPLES] Graph coloring algorithm

---
+++ emma-sketchbook/src/test/scala/eu/stratosphere/emma/examples/graphs/GraphColoringTest.scala
@@ -0,0 +1,56 @@
+package eu.stratosphere.emma.examples.graphs
+
+import java.io.File
+
+import eu.stratosphere.emma.runtime.Native
+import eu.stratosphere.emma.testutil._
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.prop.PropertyChecks
+
+import scala.io.Source
+
+@RunWith(classOf[JUnitRunner])
+class GraphColoringTest extends FunSuite with PropertyChecks with Matchers {
+  // default parameters
+  val dir   = "/graphs/graph-col"
+  val path  = tempPath(dir)
+  val rt    = Native()
+  // initialize resources
+  new File(path).mkdirs()
+  materializeResource(s"$dir/edges.tsv")
+
+  test("Merging of 2 lists of ranges should maintain order and compression") {
+    import Ordering.Implicits._
+    import GraphColoring.Schema._
+
+    forAll { (xss: List[(Int, Int)], yss: List[(Int, Int)]) =>
+      val ranges1 = compress(xss.map(r => r min r.swap).sorted)
+      val ranges2 = compress(yss.map(r => r min r.swap).sorted)
+      val merged  = merge(ranges1, ranges2)
+      val flat    = merged flatMap { case (x, y) => x :: y :: Nil }
+      merged should equal (merged.sorted)
+      flat   should equal (flat.sorted)
+    }
+  }
+
+  test("After graph coloring any two neighbors should have different colors") {
+    new GraphColoring(
+      s"$path/edges.tsv", s"$path/vertices.tsv", Native()).run()
+
+    val edges = for {
+      line <- Source.fromFile(s"$path/edges.tsv").getLines()
+      record = line split "\t" map { _.toLong }
+    } yield record(0) -> record(1)
+
+    val vertices = (for {
+      line <- Source.fromFile(s"$path/vertices.tsv").getLines()
+      record = line split "\t" map { _.toLong }
+    } yield record(0) -> record(1)).toMap
+
+    for ((src, dst) <- edges) {
+      vertices(src) should not be vertices(dst)
+    }
+  }
+}
