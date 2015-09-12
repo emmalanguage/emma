3bc38a56f010ab20ec91f1dc031224cbb0f3e1c7
Georgi Krastev <joro.kr.21@gmail.com>
2015-09-12 17:37:06 +0200

    [MACROS] Refactored `ControlFlowAnalysis`

--- emma-language/src/main/scala/eu/stratosphere/emma/macros/program/controlflow/ControlFlowAnalysis.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/macros/program/controlflow/ControlFlowAnalysis.scala
@@ -10,170 +10,177 @@ private[emma] trait ControlFlowAnalysis extends ControlFlowModel with ControlFlo
 
   def createControlFlowGraph(tree: Tree): CFGraph = {
 
-    // loop ID counter
+    // Loop ID counter
     val loopCounter = new Counter()
 
-    // vertex ID counter and an implicit next nodeID provider
+    // Vertex ID counter and an implicit next nodeID provider
     val vertexCounter = new Counter()
     implicit def nextVertexID = vertexCounter.advance.get
 
-    // initialize graph
-    val vCurr = new CFBlock()
-    val graph = Graph[CFBlock, LkDiEdge](vCurr)
+    // Initialize graph
+    val curV  = new CFBlock()
+    val graph = Graph[CFBlock, LkDiEdge](curV)
 
-    // recursive helper function
-    def _createCFG(currTree: Tree, currBlock: CFBlock): CFBlock = currTree match {
+    // Recursive helper function
+    def createCFG(curTree: Tree, curBlock: CFBlock): CFBlock = curTree match {
       // { `stats`; `expr` }
       case Block(stats, expr) =>
-        var _currBlock = currBlock
-        for (s <- stats) {
-          _currBlock = _createCFG(s, _currBlock)
-        }
-        _createCFG(expr, _currBlock)
+        var block = curBlock
+        for (stmt <- stats) block = createCFG(stmt, block)
+        createCFG(expr, block)
 
-      // val `name`: `tpt` = `rhs`
-      case ValDef(mods, name, tpt, rhs) =>
-        // ensure that the `rhs` tree is a "simple" term (no blocks or conditionals)
+      case ValDef(_, _, _, rhs) =>
+        // Ensure that the `rhs` tree is a "simple" term (no blocks or conditionals)
         if (rhs match {
-          case Block(_, _) => true
-          case If(_, _, _) => true
-          case _ => false
-        }) c.error(currTree.pos, "Emma does not support assignments with conditionals on the rhs at the moment!")
+          case _: Block => true
+          case _: If    => true
+          case _        => false
+        }) c.abort(curTree.pos,
+          "Emma does not support assignments with conditionals on the rhs at the moment")
 
-        // add assignment to the current block
-        currBlock.stats += currTree
-        currBlock
+        // Add assignment to the current block
+        curBlock.stats += curTree
+        curBlock
 
       // `name` = `rhs`
-      case Assign(Ident(name: TermName), rhs) =>
-        // ensure that the `rhs` tree is a "simple" term (no blocks or conditionals)
+      case Assign(_: Ident, rhs) =>
+        // Ensure that the `rhs` tree is a "simple" term (no blocks or conditionals)
         if (rhs match {
-          case Block(_, _) => true
-          case If(_, _, _) => true
-          case _ => false
-        }) c.error(currTree.pos, "Emma does not support assignments with conditionals on the rhs at the moment!")
+          case _: Block => true
+          case _: If    => true
+          case _        => false
+        }) c.abort(curTree.pos,
+          "Emma does not support assignments with conditionals on the rhs at the moment")
 
-        // add assignment to the current block
-        currBlock.stats += currTree
-        currBlock
+        // Add assignment to the current block
+        curBlock.stats += curTree
+        curBlock
 
       // do  { `body` } while (`cond`)
       case LabelDef(_, _, Block(b, If(cond, _, _))) =>
-        // ensure that the `cond` tree is a simple identifier
+        // Ensure that the `cond` tree is a simple identifier
+        // Should not happen after normalization
         if (cond match {
-          case Ident(_: TermName) => false
-          case _ => true
-        }) c.error(currTree.pos, "Invalid do-while test expression!") // should not happen after normalization
+          case _: Ident => false
+          case _        => true
+        }) c.abort(curTree.pos,
+          s"Invalid do-while test expression: ${showCode(cond)}")
 
-        // create body block
-        val body = if (b.size > 1) Block(b.slice(0, b.size - 1).toList, b.last) else b.head
-        val bodyStartBlock = new CFBlock()
-        val bodyEndBlock = _createCFG(body, bodyStartBlock)
+        // Create body block
+        val body       = if (b.size > 1) Block(b.init, b.last) else b.head
+        val startBlock = new CFBlock()
+        val endBlock   = createCFG(body, startBlock)
 
-        // create condition block with the `cond` term
+        // Create condition block with the `cond` term
         val condBlock = new CFBlock()
         condBlock.stats += cond
 
-        // create next block
+        // Create next block
         val nextBlock = new CFBlock()
 
-        // fix block type
-        val loopID = loopCounter.advance.get
-        bodyStartBlock.kind = DoWhileBegin(loopID)
-        bodyEndBlock.kind = DoWhileEnd(loopID)
+        // Fix block type
+        val loopID      = loopCounter.advance.get
+        startBlock.kind = DoWhileBegin(loopID)
+        endBlock.kind   = DoWhileEnd(loopID)
 
-        graph += LkDiEdge(currBlock, bodyStartBlock)(true)
-        graph += LkDiEdge(bodyEndBlock, condBlock)(true)
-        graph += LkDiEdge(condBlock, bodyStartBlock)(true)
+        graph += LkDiEdge(curBlock,  startBlock)(true)
+        graph += LkDiEdge(endBlock,  condBlock)(true)
+        graph += LkDiEdge(condBlock, startBlock)(true)
         graph += LkDiEdge(condBlock, nextBlock)(false)
 
         nextBlock
 
       // while (`cond`) { `body` }
       case LabelDef(_, _, If(cond, Block(b, _), _)) =>
-        // ensure that the `cond` tree is a simple identifier
+        // Ensure that the `cond` tree is a simple identifier
+        // Should not happen after normalization
         if (cond match {
-          case Ident(_: TermName) => false
-          case _ => true
-        }) c.error(currTree.pos, "Invalid while test expression!") // should not happen after normalization
+          case _: Ident => false
+          case _        => true
+        }) c.abort(curTree.pos,
+          s"Invalid while test expression: ${showCode(cond)}")
 
-        // create condition block with the `cond` term
+        // Create condition block with the `cond` term
         val condBlock = new CFBlock()
         condBlock.stats += cond
 
-        // create body block
-        val body = if (b.size > 1) Block(b.slice(0, b.size - 1).toList, b.last) else b.head
-        val bodyStartBlock = new CFBlock()
-        val bodyEndBlock = _createCFG(body, bodyStartBlock)
+        // Create body block
+        val body       = if (b.size > 1) Block(b.init, b.last) else b.head
+        val startBlock = new CFBlock()
+        val endBlock   = createCFG(body, startBlock)
 
-        // create next block
+        // Create next block
         val nextBlock = new CFBlock()
 
-        // fix block type
+        // Fix block type
         val loopID = loopCounter.advance.get
 
-        // fix block kinds
+        // Fix block kinds
         condBlock.kind = WhileBegin(loopID)
-        bodyEndBlock.kind = WhileEnd(loopID)
+        endBlock.kind  = WhileEnd(loopID)
 
-        graph += LkDiEdge(currBlock, condBlock)(true)
-        graph += LkDiEdge(condBlock, bodyStartBlock)(true)
+        graph += LkDiEdge(curBlock,  condBlock)(true)
+        graph += LkDiEdge(condBlock, startBlock)(true)
         graph += LkDiEdge(condBlock, nextBlock)(false)
-        graph += LkDiEdge(bodyEndBlock, condBlock)(true)
+        graph += LkDiEdge(endBlock,  condBlock)(true)
 
         nextBlock
 
-      // if (`cond`) `thenp` else `elsep`
-      case If(cond, thenp, elsep) =>
-        // ensure that the `cond` tree is a simple identifier
+      // if (`cond`) `thn` else `els`
+      case If(cond, thn, els) =>
+        // Ensure that the `cond` tree is a simple identifier
+        // Should not happen after normalization
         if (cond match {
-          case Ident(_: TermName) => true
-          case _ => false
-        }) c.error(currTree.pos, "Invalid if-then-else test expression!") // should not happen after normalization
+          case _: Ident => false
+          case _        => true
+        }) c.abort(curTree.pos,
+          s"Invalid if-then-else test expression: ${showCode(cond)}")
 
-        // create condition block with the `cond` term
+        // Create condition block with the `cond` term
         val condBlock = new CFBlock()
         condBlock.stats += cond
 
-        // create thenp block
-        val thenpStartBlock = new CFBlock()
-        val thenpEndBlock = _createCFG(thenp, thenpStartBlock)
+        // Create thn block
+        val thnStartBlock = new CFBlock()
+        val thnEndBlock   = createCFG(thn, thnStartBlock)
 
-        // create elsep block
-        val elsepStartBlock = new CFBlock()
-        val elsepEndBlock = _createCFG(elsep, elsepStartBlock)
+        // Create els block
+        val elsStartBlock = new CFBlock()
+        val elsEndBlock   = createCFG(els, elsStartBlock)
 
-        // create new current block
-        val currNewBlock = new CFBlock()
+        // Create new current block
+        val curNewBlock = new CFBlock()
 
-        // fix block kind
+        // Fix block kind
         condBlock.kind = Cond
 
-        graph += LkDiEdge(condBlock, condBlock)(true)
-        graph += LkDiEdge(condBlock, thenpStartBlock)(true)
-        graph += LkDiEdge(condBlock, elsepStartBlock)(false)
-        graph += LkDiEdge(thenpEndBlock, currNewBlock)(true)
-        graph += LkDiEdge(elsepEndBlock, currNewBlock)(true)
+        graph += LkDiEdge(condBlock,   condBlock)(true)
+        graph += LkDiEdge(condBlock,   thnStartBlock)(true)
+        graph += LkDiEdge(condBlock,   elsStartBlock)(false)
+        graph += LkDiEdge(thnEndBlock, curNewBlock)(true)
+        graph += LkDiEdge(elsEndBlock, curNewBlock)(true)
 
-        currNewBlock
+        curNewBlock
 
-      // default: a term expression
+      // Default: a term expression
       case _ =>
-        currBlock.stats += currTree
-        currBlock
+        curBlock.stats += curTree
+        curBlock
     }
 
-    // call recursive helper
-    _createCFG(tree, vCurr)
+    // Call recursive helper
+    createCFG(tree, curV)
 
-    // remove empty blocks from the end of the graph (might result from tailing if (c) e1 else e2 return statements)
-    var emptyBlocks = graph.nodes filter { x => x.diSuccessors.isEmpty && x.stats.isEmpty }
+    // Remove empty blocks from the end of the graph
+    // (might result from tailing if (c) e1 else e2 return statements)
+    var emptyBlocks = graph.nodes filter { n => n.diSuccessors.isEmpty && n.stats.isEmpty }
     while (emptyBlocks.nonEmpty) {
-      for (block <- emptyBlocks) graph -= block // remove empty block with no successors from the graph
-      emptyBlocks = graph.nodes filter { x => x.diSuccessors.isEmpty && x.stats.isEmpty }
+      // Remove empty block with no successors from the graph
+      for (block <- emptyBlocks) graph -= block
+      emptyBlocks = graph.nodes filter { n => n.diSuccessors.isEmpty && n.stats.isEmpty }
     }
 
-    // return the graph
+    // Return the graph
     graph
   }
 }
