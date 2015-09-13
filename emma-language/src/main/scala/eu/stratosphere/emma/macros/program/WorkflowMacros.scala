46a1419b5549d3c83734cd762154b53013e7bdd6
Georgi Krastev <joro.kr.21@gmail.com>
2015-09-13 15:22:09 +0200

    [MACROS] Refactored `WorkflowMacros`

--- emma-language/src/main/scala/eu/stratosphere/emma/macros/program/WorkflowMacros.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/macros/program/WorkflowMacros.scala
@@ -3,6 +3,7 @@ package eu.stratosphere.emma.macros.program
 import eu.stratosphere.emma.api.Algorithm
 import eu.stratosphere.emma.macros.program.comprehension.Comprehension
 import eu.stratosphere.emma.macros.program.controlflow.ControlFlow
+import eu.stratosphere.emma.runtime.{Engine, Native}
 import scala.language.existentials
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
@@ -10,130 +11,116 @@ import scala.reflect.macros.blackbox
 class WorkflowMacros(val c: blackbox.Context) extends ControlFlow with Comprehension {
   import universe._
 
-  /**
-   * Translates an Emma expression to an Algorithm.
-   *
-   * @return
-   */
-  def parallelize[T: c.WeakTypeTag](e: Expr[T]): Expr[Algorithm[T]] = {
+  val ENGINE = typeOf[Engine]
+  val NATIVE = typeOf[Native]
+
+  /** Translate an Emma expression to an [[Algorithm]]. */
+  // TODO: Add more comprehensive ScalaDoc
+  def parallelize[T: c.WeakTypeTag](e: Expr[T]) = {
 
     // Create a normalized version of the original tree
-    val normalizedTree = normalize(e.tree)
+    val normalized = normalize(e.tree)
 
     // ----------------------------------------------------------------------
     // Code analysis
     // ----------------------------------------------------------------------
 
     // 1. Create control flow graph
-    implicit var cfGraph = createControlFlowGraph(normalizedTree)
+    implicit var cfGraph = createControlFlowGraph(normalized)
 
     // 2. Identify and isolate maximal comprehensions
-    implicit var comprehensionView = createComprehensionView(cfGraph)
+    implicit var compView = createComprehensionView(cfGraph)
 
     // ----------------------------------------------------------------------
     // Code optimizations
     // ----------------------------------------------------------------------
 
     // 1. Inline comprehensions
-    val optimizedTree = inlineComprehensions(normalizedTree)
-    cfGraph = createControlFlowGraph(optimizedTree)
-    comprehensionView = createComprehensionView(cfGraph)
+    val optimized = inlineComprehensions(normalized)
+    cfGraph       = createControlFlowGraph(optimized)
+    compView      = createComprehensionView(cfGraph)
 
-    // normalize filter predicates to CNF
-    normalizePredicates(optimizedTree)
+    // 2. Normalize filter predicates to CNF
+    normalizePredicates(optimized)
 
-    // 2. Apply Fold-Group-Fusion where possible
-    foldGroupFusion(optimizedTree)
+    // 3. Apply Fold-Group-Fusion where possible
+    foldGroupFusion(optimized)
 
     // ----------------------------------------------------------------------
     // Final object assembly
     // ----------------------------------------------------------------------
 
-    // construct algorithm object
-    val algorithmCode =
-      q"""
-      new eu.stratosphere.emma.api.Algorithm[${c.weakTypeOf[T]}] {
-         import _root_.scala.reflect._
+    // Construct algorithm object
+    q"""new _root_.eu.stratosphere.emma.api.Algorithm[${weakTypeOf[T]}] {
+      import _root_.scala.reflect._
 
-         def run(engine: eu.stratosphere.emma.runtime.Engine): ${c.weakTypeOf[T]} = engine match {
-           case _: eu.stratosphere.emma.runtime.Native => runNative()
-           case _ => runParallel(engine)
-         }
-
-         private def runNative(): ${c.weakTypeOf[T]} = {
-           ${e.tree.unTypeChecked}
-         }
-
-         private def runParallel(engine: eu.stratosphere.emma.runtime.Engine): ${c.weakTypeOf[T]} = {
-           ${compile(optimizedTree, cfGraph, comprehensionView)}
-         }
+      def run(engine: $ENGINE): ${weakTypeOf[T]} = engine match {
+        case _: $NATIVE => runNative()
+        case _ => runParallel(engine)
       }
-      """
 
-    c.Expr[Algorithm[T]](c.typecheck(algorithmCode))
+      private def runNative(): ${weakTypeOf[T]} = {
+        ${e.tree.unTypeChecked}
+      }
+
+      private def runParallel(engine: $ENGINE): ${weakTypeOf[T]} = {
+        ${compile(optimized, cfGraph, compView)}
+      }
+    }""".typeChecked
   }
 
-  /**
-   * Translates an Emma expression to an Algorithm.
-   *
-   * @return
-   */
-  def comprehend[T: c.WeakTypeTag](e: Expr[T]): Expr[Unit] = {
+  /** Translate an Emma expression to an [[Algorithm]]. */
+  // TODO: Add more comprehensive ScalaDoc
+  def comprehend[T: c.WeakTypeTag](e: Expr[T]) = {
 
     // Create a normalized version of the original tree
-    val normalizedTree = normalize(e.tree)
+    val normalized = normalize(e.tree)
 
     // ----------------------------------------------------------------------
     // Code analysis
     // ----------------------------------------------------------------------
 
     // 1. Create control flow graph
-    implicit var cfGraph = createControlFlowGraph(normalizedTree)
+    implicit var cfGraph = createControlFlowGraph(normalized)
 
     // 2. Identify and isolate maximal comprehensions
-    implicit var comprehensionView = createComprehensionView(cfGraph)
+    implicit var compView = createComprehensionView(cfGraph)
 
     // ----------------------------------------------------------------------
     // Code optimizations
     // ----------------------------------------------------------------------
 
     // 1. Inline comprehensions
-    val optimizedTree = inlineComprehensions(normalizedTree)
-    cfGraph = createControlFlowGraph(optimizedTree)
-    comprehensionView = createComprehensionView(cfGraph)
+    val optimized = inlineComprehensions(normalized)
+    cfGraph       = createControlFlowGraph(optimized)
+    compView      = createComprehensionView(cfGraph)
 
-    // 2. Apply Fold-Group-Fusion where possible
-    foldGroupFusion(optimizedTree)
-    normalizePredicates(optimizedTree)
+    // 2. Normalize filter predicates to CNF
+    normalizePredicates(optimized)
+
+    // 3. Apply Fold-Group-Fusion where possible
+    foldGroupFusion(optimized)
 
     // ----------------------------------------------------------------------
     // Final result assembly
     // ----------------------------------------------------------------------
 
-    val code =
-      q"""
-      { ..${
-        for (t <- comprehensionView.terms) yield
-        q"""
-          println("~" * 80)
-          println("~ Comprehension `" + ${t.id.toString} + "` (original):")
-          println("~" * 80)
-          println(${t.comprehension.toString})
-          println("~" * 80)
-          println("~ Comprehension `" + ${t.id.toString} + "` (combinatros):")
-          println("~" * 80)
-          println(${combine(t.comprehension).toString})
-          println("~" * 80)
-          println("")
-          println("~ Comprehension `" + ${t.id.toString} + "` (IR):")
-          println("~" * 80)
-          println(${showCode(compile(optimizedTree, cfGraph, comprehensionView))})
-          println("~" * 80)
-          println("")
-          """
-      }}"""
-
-
-    c.Expr[Unit](c.typecheck(code))
+    q"""{ ..${for (term <- compView.terms) yield q"""
+      println("~" * 80)
+      println("~ Comprehension `" + ${term.id.toString} + "` (original):")
+      println("~" * 80)
+      println(${term.comprehension.toString})
+      println("~" * 80)
+      println("~ Comprehension `" + ${term.id.toString} + "` (combinatros):")
+      println("~" * 80)
+      println(${combine(term.comprehension).toString})
+      println("~" * 80)
+      println("")
+      println("~ Comprehension `" + ${term.id.toString} + "` (IR):")
+      println("~" * 80)
+      println(${showCode(compile(optimized, cfGraph, compView))})
+      println("~" * 80)
+      println("")"""}
+    }""".typeChecked
   }
 }
