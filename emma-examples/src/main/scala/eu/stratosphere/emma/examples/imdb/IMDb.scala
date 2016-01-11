43515b395c1cfcf9b0f5fbf31b287a61e29f8590
Georgi Krastev <joro.kr.21@gmail.com>
2016-01-11 20:49:06 +0100

    [EXAMPLES] Added simple IMDb example

---
+++ emma-examples/src/main/scala/eu/stratosphere/emma/examples/imdb/IMDb.scala
@@ -0,0 +1,109 @@
+package eu.stratosphere.emma.examples.imdb
+
+import eu.stratosphere.emma.api._
+import eu.stratosphere.emma.examples.Algorithm
+import eu.stratosphere.emma.runtime.Engine
+import net.sourceforge.argparse4j.inf.{Namespace, Subparser}
+
+class IMDb(input: String, output: String, rt: Engine) extends Algorithm(rt) {
+  import IMDb._
+  import Schema._
+  import Ordering._
+
+  def this(ns: Namespace, rt: Engine) = this(
+    ns.get(IMDb.Command.keyInput),
+    ns.get(IMDb.Command.keyOutput),
+    rt)
+
+  def run() = algorithm.run(rt)
+
+  val algorithm = emma.parallelize {
+    // let's crunch some numbers first
+    val numbers = DataBag(1 to 1000)
+
+    // find pythagorean triangles
+    val pythagoras = for {
+      a <- numbers
+      b <- numbers
+      c <- numbers
+      if a <= b && b <= c
+      if a*a + b*b == c*c
+    } yield (a, b, c)
+
+    for { (a, b, c) <- pythagoras.fetch() }
+      println(s"$a^2 + $b^2 == $c^2")
+
+    // read in some real data
+    val movies = read(s"$input/imdb.csv", new CSVInputFormat[Movie])
+    val cannes = read(s"$input/canneswinners.csv", new CSVInputFormat[Winner])
+    val berlin = read(s"$input/berlinalewinners.csv", new CSVInputFormat[Winner])
+
+    // who won the festivals?
+    val winners = for {
+      movie <- movies
+      winner <- cannes plus berlin
+      if movie.title == winner.title
+    } yield movie
+
+    for { winner <- winners.bottom(10)(by { _.rating }) }
+      println(winner)
+
+    // who directed the winners?
+    val filmographies = for {
+      movie <- movies
+      winner <- cannes plus berlin
+      if movie.title == winner.title
+    } yield winner.director -> movie
+
+    // how many festivals did each director win?
+    val festivalsWon = for {
+      filmography <- filmographies
+        .groupBy { case (director, movie) => director }
+    } yield filmography.key -> filmography.values.count()
+
+    for { (director, wins) <- festivalsWon.fetch() }
+      println(s"Director $director won $wins festivals.")
+
+    // write the results
+    write(s"$output/triangles.csv", new CSVOutputFormat[(Int, Int, Int)])(pythagoras)
+    write(s"$output/winners.csv", new CSVOutputFormat[Movie])(winners)
+    write(s"$output/festivalswon.csv", new CSVOutputFormat[(String, Long)])(festivalsWon)
+  }
+}
+
+object IMDb {
+  class Command extends Algorithm.Command[IMDb] {
+    import Command._
+
+    override def name = "imdb"
+
+    override def description =
+      "Play around with IMDb movies and film festivals."
+
+    override def setup(parser: Subparser) = {
+      super.setup(parser)
+
+      parser.addArgument(keyInput)
+        .`type`(classOf[String])
+        .dest(keyInput)
+        .metavar("INPUT")
+        .help("input directory")
+
+      parser.addArgument(keyOutput)
+        .`type`(classOf[String])
+        .dest(keyOutput)
+        .metavar("OUTPUT")
+        .help("output directory")
+    }
+  }
+
+  object Command {
+    val keyInput  = "input"
+    val keyOutput = "output"
+  }
+
+  object Schema {
+    case class Movie(title: String, rating: Double, rank: Int, link: String, year: Int)
+    case class Winner(year: Int, title: String, director: String, country: String)
+  }
+}
---
+++ emma-examples/src/test/scala/eu/stratosphere/emma/examples/imdb/IMDbTest.scala
@@ -0,0 +1,33 @@
+package eu.stratosphere.emma.examples.imdb
+
+import eu.stratosphere.emma.testutil._
+
+import java.io.File
+
+import org.junit.experimental.categories.Category
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+@Category(Array(classOf[ExampleTest]))
+@RunWith(classOf[JUnitRunner])
+class IMDbTest extends FunSuite with Matchers with BeforeAndAfter {
+  // default parameters
+  val dir = "/cinema"
+  val path = tempPath(dir)
+
+  before {
+    new File(path).mkdirs()
+    materializeResource(s"$dir/imdb.csv")
+    materializeResource(s"$dir/berlinalewinners.csv")
+    materializeResource(s"$dir/canneswinners.csv")
+  }
+
+  after {
+    deleteRecursive(new File(path))
+  }
+
+  test("IMDb") (withRuntime() { rt =>
+    new IMDb(path, s"$path/output", rt).run()
+  })
+}
