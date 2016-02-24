24291f651fd66322e23d10b68a9dd97e94d8996d
Georgi Krastev <joro.kr.21@gmail.com>
2016-02-24 16:26:48 +0100

    [MACROS] Refactored the CSV convertion derivation
    * Introduced a functional constructor in object `CSVConverters`
    * Refactored the macro to use lambdas instead of anonymous classes

--- emma-common-macros/src/main/scala/eu/stratosphere/emma/api/CSVConverters.scala
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/api/CSVConverters.scala
@@ -1,6 +1,28 @@
-package eu.stratosphere.emma.api
+package eu.stratosphere
+package emma.api
 
+/** Serialization format for encoding to / decoding from CSV records. */
 trait CSVConverters[T] {
-  def fromCSV(value: Array[String]): T
-  def toCSV(values: T, separator: Char): Array[String]
+
+  /** Parses and returns the value encoded in `record`. */
+  def fromCSV(record: Array[String]): T
+
+  /** Serializes `value` using `sep` as field delimiter. */
+  def toCSV(value: T, sep: Char): Array[String]
+}
+
+object CSVConverters {
+
+  def apply[T](from: Array[String] => T)
+    (to: (T, Char) => Array[String]): CSVConverters[T] = {
+
+    new CSVConverters[T] {
+
+      override def fromCSV(record: Array[String]): T =
+        from(record)
+
+      override def toCSV(value: T, sep: Char): Array[String] =
+        to(value, sep)
+    }
+  }
 }
