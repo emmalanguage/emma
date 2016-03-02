426008e3104b7a7636517a0129705e43133c89ff
Andreas Salzmann <redshark_123@web.de>
2016-03-02 13:33:03 +0100

    [GUI] Scala style changes.

--- emma-gui/src/main/scala/org/emma/runtime/ExampleRunner.scala
+++ emma-gui/src/main/scala/org/emma/runtime/ExampleRunner.scala
@@ -9,12 +9,13 @@ import eu.stratosphere.emma.runtime.{Flink, Engine, RuntimePlugin}
 import net.sourceforge.argparse4j.inf.Namespace
 
 class ExampleRunner(constructor: Constructor[Algorithm], args: Namespace,
-                    plugins: Seq[RuntimePlugin] = Nil, runtime: Engine = new Flink) extends Thread {
+    plugins: Seq[RuntimePlugin] = Nil, runtime: Engine = new Flink) extends Thread {
 
-  override def run() {
+  override def run() : Unit = {
     runtime.plugins = plugins
     try constructor.newInstance(args, runtime).run() catch {
-      case e: InvocationTargetException if e.getCause.isInstanceOf[ClassCastException] => throw new InvalidParameterException("Given parameter types do not match the example constructor.")
+      case e: InvocationTargetException if e.getCause.isInstanceOf[ClassCastException] =>
+        throw new InvalidParameterException("Given parameter types do not match the example constructor.")
       case e: Exception => e.printStackTrace()
     }
   }
