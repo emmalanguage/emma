5ead38265d4e2d0fce550594adf3cb9c23ee3d7d
Johann Kovacs <me@jkovacs.de>
2016-04-08 16:10:33 +0200

    Local schema analysis (resolves #179)

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/ir/lnf/SchemaOptimizations.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/ir/lnf/SchemaOptimizations.scala
@@ -2,16 +2,25 @@ package eu.stratosphere.emma.compiler.ir.lnf
 
 import eu.stratosphere.emma.compiler.ir.CommonIR
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scalax.collection.GraphEdge.UnDiEdge
+import scalax.collection.immutable.Graph
+
+
 trait SchemaOptimizations extends CommonIR {
   self: Language =>
 
   import universe._
+  import Tree._
 
   // ---------------------------------------------------------------------------
   // Schema Analysis & Optimizations
   // ---------------------------------------------------------------------------
 
   object Schema {
+
+    import CaseClassMeta._
 
     /** Root trait for all fields. */
     sealed trait Field
@@ -29,7 +38,7 @@ trait SchemaOptimizations extends CommonIR {
     case class Info(fieldClasses: Set[FieldClass])
 
     /**
-     * Compute the (local) schema information for a tree fragment.
+     * Compute the (global) schema information for a tree fragment.
      *
      * @param tree The ANF [[Tree]] to be analyzed.
      * @return The schema information for the input tree.
@@ -39,14 +48,102 @@ trait SchemaOptimizations extends CommonIR {
     }
 
     /**
-     * Compute the (global) schema information for an anonymous function.
+     * Compute the (local) schema information for an anonymous function.
      *
      * @param tree The ANF [[Tree]] to be analyzed.
      * @return The schema information for the input tree.
      */
     private[emma] def local(tree: Function): Schema.Info = {
-      Info(Set.empty[FieldClass])
+      // initialize equivalences relation
+      val equivalences: mutable.Buffer[(Field, Field)] = ArrayBuffer()
+
+      // traverse the function and collect equivalences
+      traverse(tree) {
+        // patterns of type `x = y.target`
+        case val_(x, Select(y, member@TermName(_)), _) =>
+          equivalences += SimpleField(x) -> MemberField(Term.of(y), Term.member(y, member))
+          equivalences ++= caseClassMemberEquivalences(x)
+
+        // patterns of type `x = constructor (arg1, ..., argN)`
+        case val_(x, Apply(fun, args), _) if isCaseClassConstructor(fun.symbol) =>
+          equivalences += SimpleField(x) -> SimpleField(x)
+
+          // add constructor equivalences and members
+          val meta = CaseClassMeta(Type.of(x).typeSymbol)
+          equivalences ++= args.zip(meta.constructorProjections(fun.symbol)).map {
+            case (arg, param) => SimpleField(arg.symbol) -> MemberField(x, param)
+          }
+          equivalences ++= meta.memberEquivalences(x)
+
+        // just a regular ValDef
+        case val_(x, _, _) =>
+          equivalences += SimpleField(x) -> SimpleField(x)
+          equivalences ++= caseClassMemberEquivalences(x)
+
+      }
+
+      Info(equivalenceClasses(equivalences))
     }
+
+    private def equivalenceClasses(equivalences: Seq[(Field, Field)]): Set[FieldClass] = {
+      val edges = equivalences.map { case (f, t) => UnDiEdge(f, t) }
+      val nodes = edges.flatMap(_.toSet)
+      val graph = Graph.from[Field, UnDiEdge](nodes, edges)
+
+      // each connected component forms an equivalence class
+      graph.componentTraverser().map(_.nodes.map(_.value)).toSet
+    }
+
+    class CaseClassMeta(sym: ClassSymbol) {
+
+      // assert that the input argument is a case class symbol
+      assert(sym.isCaseClass)
+
+      def constructorProjections(fun: Symbol): Seq[Symbol] = {
+        assert(isCaseClassConstructor(fun))
+        val parameters = fun.asMethod.paramLists.head
+        parameters.map(p => sym.info.decl(p.name))
+      }
+
+      def members(): Set[Symbol] = {
+        Type.of(sym).members.filter {
+          case m: TermSymbol => m.isGetter && m.isCaseAccessor
+        }.toSet
+      }
+
+      def memberFields(symbol: Symbol): Set[MemberField] = {
+        members().map(member => MemberField(symbol, member))
+      }
+
+      def memberEquivalences(symbol: Symbol): Set[(Field, Field)] = {
+        memberFields(symbol).map(f => f -> f)
+      }
+    }
+
+    object CaseClassMeta {
+
+      def apply(s: Symbol) = new CaseClassMeta(s.asClass)
+
+      def isCaseClass(s: Symbol): Boolean = s.isClass && s.asClass.isCaseClass
+
+      def isCaseClassConstructor(fun: Symbol): Boolean = {
+        assert(fun.isMethod)
+        val isCtr = fun.owner.companion.isModule && fun.isConstructor
+        val isApp = fun.owner.isModuleClass && fun.name == TermName("apply") && fun.isSynthetic
+
+        isCtr || isApp
+      }
+
+      def caseClassMemberEquivalences(s: Symbol): Set[(Field, Field)] = {
+        val tpe = Type.of(s).typeSymbol
+        if (isCaseClass(tpe)) {
+          CaseClassMeta(tpe).memberEquivalences(s)
+        } else {
+          Set.empty
+        }
+      }
+    }
+
   }
 
 }
