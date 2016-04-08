23d6551a40aa09d42acdb38aa39fbffc90aaa891
Johann Kovacs <me@jkovacs.de>
2016-04-08 16:10:33 +0200

    Local schema analysis (resolves #179)

--- emma-language/src/test/scala/eu/stratosphere/emma/compiler/ir/lnf/SchemaInfoSpec.scala
+++ emma-language/src/test/scala/eu/stratosphere/emma/compiler/ir/lnf/SchemaInfoSpec.scala
@@ -24,9 +24,69 @@ class SchemaInfoSpec extends BaseCompilerSpec {
     case vd@ValDef(_, TermName(`name`), _, _) => vd.symbol
   }.head
 
+  "tuple tupes playground" - {
+
+    import compiler.Type
+
+    val examples = Seq(
+      typeCheck(reify {
+        (42, "foobar")
+      }),
+      typeCheck(reify {
+        new Tuple2(42, "foobar")
+      }),
+      typeCheck(reify {
+        Ad(1, "foobar", AdClass.FASHION)
+      }),
+      typeCheck(reify {
+        new Ad(1, "foobar", AdClass.FASHION)
+      })
+    )
+
+    // extract the types of the objects constructed the examples
+    val types = for (e <- examples) yield {
+      Type.of(e).typeSymbol.asClass
+    }
+
+    // assert that all are case classes
+    for (e <- types) assert(e.isCaseClass)
+
+    // extract the symbols of the constructing functions
+    val funsyms = for (e <- examples) yield e match {
+      case Apply(fun, args) => fun.symbol
+    }
+
+    // assert that the funsyms belong to recognized constructors
+    val res = for ((f, t) <- funsyms zip types) {
+      // the owning class of the function symbol should be the class of the target
+      val cmp = f.owner.companionSymbol
+      val cls = cmp.companion
+      assert(cls == t)
+
+      // the function symbol should be either of a constructor of of a synthetic apply method
+      val isCtr = f.owner.companionSymbol.isModule && f.isConstructor
+      val isApp = f.owner.isModuleClass && f.name == TermName("apply") && f.isSynthetic
+
+      assert(isCtr || isApp)
+    }
+
+    // extract the projections associated with the function applications
+    val proj = for (f <- funsyms) yield {
+      // the owning class of the function symbol should be the class of the target
+      val cmp = f.owner.companionSymbol
+      val cls = cmp.companion
+
+      for (param <- f.paramLists.head) yield {
+        val proj = cls.info.decl(param.name)
+        assert(proj.isAccessor)
+        (param, cls.info.decl(param.name))
+      }
+    }
+  }
+
   "local schema" - {
     "without control flow" in {
-      // ANF representation with `desugared` comprehensinos
+      // ANF representation with `desugared` comprehensions
       val fn = typeCheck(reify {
         (c: Click) => {
           val t = c.time
@@ -61,10 +121,10 @@ class SchemaInfoSpec extends BaseCompilerSpec {
       val cls$04 /*      */ = Set[Field](fld$c$time, fld$t)
       val cls$05 /*      */ = Set[Field](fld$p, fld$a$_2)
       val cls$06 /*      */ = Set[Field](fld$m, fld$a$_3)
-      val cls$v7 /*      */ = Set[Field](fld$a)
+      val cls$07 /*      */ = Set[Field](fld$a)
 
       // 3) construct the expected local schema information
-      val exp = Info(Set(cls$01, cls$02, cls$03, cls$01, cls$01, cls$01, cls$01))
+      val exp = Info(Set(cls$01, cls$02, cls$03, cls$04, cls$05, cls$06, cls$07))
 
       // compute actual local schema
       val act = compiler.Schema.local(fn)
