69a3504446c97baba48fb0f6e9d592f30cc5c53c
Georgi Krastev <joro.kr.21@gmail.com>
2016-04-19 15:12:59 +0200

    Revert "[MACROS] Refactored the CSV convertion derivation"
    This reverts commit 9a34300f3b0ecb9c84eafd626c473acf49e5db97.

--- emma-common-macros/src/main/scala/eu/stratosphere/emma/api/CSVConverters.scala
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/api/CSVConverters.scala
@@ -1,28 +1,6 @@
-package eu.stratosphere
-package emma.api
+package eu.stratosphere.emma.api
 
-/** Serialization format for encoding to / decoding from CSV records. */
 trait CSVConverters[T] {
-
-  /** Parses and returns the value encoded in `record`. */
-  def fromCSV(record: Array[String]): T
-
-  /** Serializes `value` using `sep` as field delimiter. */
-  def toCSV(value: T, sep: Char): Array[String]
-}
-
-object CSVConverters {
-
-  def apply[T](from: Array[String] => T)
-    (to: (T, Char) => Array[String]): CSVConverters[T] = {
-
-    new CSVConverters[T] {
-
-      override def fromCSV(record: Array[String]): T =
-        from(record)
-
-      override def toCSV(value: T, sep: Char): Array[String] =
-        to(value, sep)
-    }
-  }
+  def fromCSV(value: Array[String]): T
+  def toCSV(values: T, separator: Char): Array[String]
 }
