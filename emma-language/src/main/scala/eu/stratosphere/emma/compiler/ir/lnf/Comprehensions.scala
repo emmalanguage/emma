38e94e2b2f1052a2fcd2071b897137c9113a4ee1
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-04-24 20:57:48 +0200

    [IR] Comprehension normalization.
    This commit migrates and adapts the normalization logic
    from the old `ComprehensionNormalization` engine as a
    `Comprehension.normalize` transformation.

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/ir/lnf/Comprehensions.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/ir/lnf/Comprehensions.scala
@@ -1,6 +1,8 @@
 package eu.stratosphere.emma.compiler.ir.lnf
 
-trait Comprehensions {
+import eu.stratosphere.emma.compiler.ir.Rewrite
+
+trait Comprehensions extends Rewrite {
   self: Language =>
 
   import universe._
@@ -202,7 +204,26 @@ trait Comprehensions {
      * @return The input [[Tree]] with resugared comprehensions.
      */
     def normalize(monad: Symbol)(tree: Tree): Tree = {
-      tree
+      // construct comprehension syntax helper for the given monad
+      val cs = new Syntax(monad: Symbol)
+
+      ({
+        // apply UnnestHead and UnnestGenerator rules exhaustively
+        Engine.postWalk(List(cs.UnnestHead, cs.UnnestGenerator))
+      } andThen {
+        // elminiate dead code produced by normalization
+        LNF.dce
+      } andThen {
+        // elminiate trivial guards produced by normalization
+        postWalk {
+          case cs.comprehension(qs, hd) =>
+            cs.comprehension(
+              qs filterNot {
+                case t@cs.guard(block(_, Literal(Constant(true)))) => true
+                case t => false
+              }, hd)
+        }
+      }) (tree)
     }
 
     // -------------------------------------------------------------------------
@@ -353,6 +374,281 @@ trait Comprehensions {
             if Term.sym(fun) == symbol => Some(expr)
           case _ =>
             None
+        }
+      }
+
+      // -------------------------------------------------------------------------
+      // Comprehension normalization rules
+      // -------------------------------------------------------------------------
+
+      /**
+       * Unnests a comprehended head in its parent.
+       *
+       * ==Matching Pattern==
+       * {{{
+       * {
+       *   $bs1
+       *   val c = flatten {
+       *     $bs2
+       *     comprehension {
+       *       $qs1
+       *       head {
+       *         $bs3
+       *         comprehension {
+       *           $qs2
+       *           head $hd
+       *         } // child comprehension
+       *       }
+       *     } // outer comprehension
+       *   } // parent expression
+       *   $bs4
+       * } // enclosing block
+       * }}}
+       *
+       * ==Guard==
+       * None.
+       *
+       * ==Rewrite==
+       *
+       * Let $bs3 decompose into the following two subsets:
+       * - $bs3i (transitively) depends on symbols defined in $qs1, and
+       * - $bs3o is the independent complement $bs3 \ $bs3o.
+       *
+       * {{{
+       * {
+       *   $bs1
+       *   $bs2
+       *   $bs3o
+       *   val c = comprehension {
+       *     $qs1
+       *     $qs2' // where let blocks are prefixed with $bs3i
+       *     head $hd' // where let blocks are prefixed with $bs3i
+       *   } // flattened result comprehension
+       *   $bs4
+       * } // enclosing block
+       * }}}
+       */
+      object UnnestHead extends Rule {
+
+        case class RuleMatch(enclosing: Block, parent: Tree, child: Block) {
+          lazy val bs1 = enclosing.children.takeWhile(_ != parent)
+          lazy val bs2 = parent match {
+            case val_(_, flatten(block(bs, comprehension(_, _))), _) => bs
+            case flatten(block(bs, comprehension(_, _))) => bs
+          }
+          lazy val bs3 = child.stats
+          lazy val bs4 = enclosing.children.reverse.takeWhile(_ != parent).reverse
+          lazy val qs1 = parent match {
+            case val_(_, flatten(block(Nil, comprehension(qs, _))), _) => qs
+            case flatten(block(Nil, comprehension(qs, _))) => qs
+          }
+          lazy val (qs2, hd) = child.expr match {
+            case comprehension(_qs2, head(_hd)) => (_qs2, _hd)
+          }
+          lazy val (bs3i, bs3o) = {
+            val surrogateSym = (t: Tree) =>
+              Term.sym.free(Term.name.fresh("x"), t.tpe)
+
+            val bs3refs = (bs3 map {
+              case val_(sym, rhs, _) => sym -> Tree.refs(rhs)
+              case stmt => surrogateSym(stmt) -> Tree.refs(stmt)
+            }).toMap
+
+            val qs1defs = (qs1 flatMap {
+              case generator(sym, _) => List(sym)
+              case guard(_) => List.empty[TermSymbol]
+            }).toSet
+
+            var bs3iSym = for {
+              sym <- bs3refs.keySet
+              if (bs3refs(sym) intersect qs1defs).nonEmpty
+            } yield sym
+
+            var delta = Set.empty[TermSymbol]
+            do {
+              bs3iSym = bs3iSym union delta
+              delta = (for {
+                sym <- bs3refs.keySet
+                if (bs3refs(sym) intersect bs3iSym).nonEmpty
+              } yield sym) diff bs3iSym
+            } while (delta.nonEmpty)
+
+            val bs3oSym = bs3refs.keySet diff bs3iSym
+
+            val bs3i = bs3 flatMap {
+              case vd@val_(sym, _, _) if bs3iSym contains sym => List(vd)
+              case _ => Nil
+            }
+            val bs3o = bs3 flatMap {
+              case vd@val_(sym, _, _) if bs3oSym contains sym => List(vd)
+              case _ => Nil
+            }
+
+            (bs3i, bs3o)
+          }
+        }
+
+        override def bind(root: Tree): Traversable[RuleMatch] = root match {
+          case enclosing: Block => new Traversable[RuleMatch] {
+            override def foreach[U](f: (RuleMatch) => U): Unit = {
+              val parents = (enclosing.stats collect {
+                // a ValDef statement with a `flatten(comprehension(...))` rhs
+                case parent@val_(_, flatten(block(_, outer@comprehension(_, _))), _) => (parent, outer)
+              }) ++ (enclosing.expr match {
+                // an expr with a `flatten(comprehension(...))`
+                case parent@flatten(block(_, outer@comprehension(_, _))) => List((parent, outer))
+                case _ => Nil
+              })
+
+              for ((parent, outer) <- parents) outer match {
+                case comprehension(_, head(child@Block(_, comprehension(_, _)))) =>
+                  f(RuleMatch(enclosing, parent, child))
+              }
+            }
+          }
+          case _ => Traversable.empty[RuleMatch]
+        }
+
+        override def guard(rm: RuleMatch): Boolean =
+          true
+
+        override def fire(rm: RuleMatch): Tree = {
+          // construct a flattened version of the parent comprehension
+          val flattened = rm.parent match {
+            case val_(vsym, flatten(block(_, comprehension(_, _))), _) =>
+              val_(vsym,
+                comprehension(
+                  rm.qs1 ++ rm.qs2 collect {
+                    case generator(sym, rhs) =>
+                      Syntax.this.generator(sym, prepend(rm.bs3i, rhs))
+                    case guard(expr) =>
+                      Syntax.this.guard(prepend(rm.bs3i, expr))
+                  },
+                  head(prepend(rm.bs3i, rm.hd))))
+            case flatten(block(_, comprehension(_, _))) =>
+              comprehension(
+                rm.qs1 ++ rm.qs2 collect {
+                  case generator(sym, rhs) =>
+                    Syntax.this.generator(sym, prepend(rm.bs3i, rhs))
+                  case guard(expr) =>
+                    Syntax.this.guard(prepend(rm.bs3i, expr))
+                },
+                head(prepend(rm.bs3i, rm.hd)))
+          }
+
+          // construct and return a new enclosing block
+          block(rm.bs1 ++ rm.bs2 ++ rm.bs3o ++ List(flattened) ++ rm.bs4)
+        }
+
+        private def prepend(prefix: List[ValDef], blck: Block): Block =
+          Tree.refresh(block(prefix, blck.children: _*), prefix collect {
+            case val_(sym, _, _) => sym
+          }: _*).asInstanceOf[Block]
+      }
+
+      /**
+       * Un-nests a comprehended generator in its parent.
+       *
+       * ==Matching Pattern==
+       * {{{
+       * {
+       *   $bs1
+       *   val y = comprehension {
+       *     $qs2
+       *     head $hd2
+       *   } // child comprehension
+       *   $bs2
+       *   comprehension {
+       *     $qs1
+       *     val x = generator(y) // gen
+       *     $qs3
+       *     head $hd1
+       *   } // parent comprehension
+       * } // enclosing block
+       * }}}
+       *
+       * ==Guard==
+       * None.
+       *
+       * ==Rewrite==
+       * {{{
+       * {
+       *   $bs1
+       *   $bs2
+       *   comprehension {
+       *     $qs1
+       *     $qs2
+       *     $qs3 [ $hd2 \ x ]
+       *     head $hd1 [ $hd2 \ x ]
+       *   } // unnested result comprehension
+       * } // enclosing block
+       * }}}
+       */
+      object UnnestGenerator extends Rule {
+
+        case class RuleMatch(enclosing: Block, parent: Tree, gen: ValDef, child: ValDef) {
+          lazy val bs1 = enclosing.stats.takeWhile(_ != child)
+          lazy val bs2 = enclosing.stats.reverse.takeWhile(_ != child).reverse
+          lazy val qs1 = parent match {
+            case comprehension(qs, _) => qs.takeWhile(_ != gen)
+          }
+          lazy val qs2 = child match {
+            case val_(_, comprehension(qs, _), _) => qs
+          }
+          lazy val qs3 = parent match {
+            case comprehension(qs, _) => qs.reverse.takeWhile(_ != gen).reverse
+          }
+          lazy val hd1 = parent match {
+            case comprehension(_, head(hd)) => hd match {
+              case block(Nil, expr) => expr
+              case other => other
+            }
+          }
+          lazy val hd2 = child match {
+            case val_(_, comprehension(_, head(hd)), _) => hd match {
+              case block(Nil, expr) => expr
+              case other => other
+            }
+          }
+        }
+
+        def bind(root: Tree): Traversable[RuleMatch] = root match {
+          case enclosing: Block => new Traversable[RuleMatch] {
+            override def foreach[U](f: (RuleMatch) => U): Unit = {
+              // compute a lookup table for comprehensions
+              // defined in the current enclosing bock
+              val lookup = (enclosing.stats collect {
+                case vd@val_(sym, rhs@comprehension(_, _), _) => sym -> vd
+              }).toMap
+
+              if (lookup.nonEmpty) enclosing.expr match {
+                case parent@comprehension(qs, hd) => qs collect {
+                  case gen@generator(sym, block(_, rhs: Ident)) =>
+                    for (child <- lookup.get(Term sym rhs))
+                      f(RuleMatch(enclosing, parent, gen, child))
+                }
+                case _ => Unit
+              }
+            }
+          }
+          case _ => Traversable.empty[RuleMatch]
+        }
+
+        def guard(rm: RuleMatch): Boolean =
+          true
+
+        def fire(rm: RuleMatch): Tree = {
+          // define a substitution function `· [ $hd2 \ x ]`
+          val subst: Tree => Tree = Tree.subst(_, Map(rm.gen.symbol -> rm.hd2))
+
+          // construct an unnested version of the parent comprehension
+          val unnested = comprehension(
+            rm.qs1 ++ rm.qs2 ++ (rm.qs3 map subst),
+            head(asBlock(subst(rm.hd1)))
+          )
+
+          // construct and return a new enclosing block
+          block(rm.bs1 ++ rm.bs2 ++ List(unnested))
         }
       }
 
---
+++ emma-language/src/test/scala/eu/stratosphere/emma/compiler/ir/lnf/comprehensions/NormalizationSpec.scala
@@ -0,0 +1,374 @@
+package eu.stratosphere.emma.compiler.ir.lnf.comprehensions
+
+import java.time.Instant
+
+import eu.stratosphere.emma.api.DataBag
+import eu.stratosphere.emma.compiler.BaseCompilerSpec
+import eu.stratosphere.emma.compiler.ir._
+import eu.stratosphere.emma.compiler.ir.lnf.TreeEquality
+import eu.stratosphere.emma.testschema.Marketing._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/** A spec for resugaring comprehensions. */
+@RunWith(classOf[JUnitRunner])
+class NormalizationSpec extends BaseCompilerSpec with TreeEquality {
+
+  import compiler._
+  import universe._
+
+  // ---------------------------------------------------------------------------
+  // Helper methods
+  // ---------------------------------------------------------------------------
+
+  /** Pipeline for this spec. */
+  def typeCheckAndANF[T]: Expr[T] => Tree = {
+    (_: Expr[T]).tree
+  } andThen {
+    Type.check(_: Tree)
+  } andThen {
+    LNF.destructPatternMatches
+  } andThen {
+    LNF.resolveNameClashes
+  } andThen {
+    LNF.anf
+  } andThen {
+    LNF.simplify
+  } andThen {
+    Comprehension.resugar(API.bagSymbol)
+  } andThen {
+    Owner.at(Owner.enclosing)
+  }
+
+  /** Normalize mock-comprehension selection chains. */
+  def fix: Tree => Tree = tree => {
+    val moduleSel = Tree.resolve(IR.module)
+    preWalk(tree) {
+      case s@Select(_, name) if IR.comprehensionOps contains s.symbol.asTerm =>
+        Term.sel(moduleSel, s.symbol.asTerm)
+    }
+  }
+
+  def normalize: Tree => Tree = {
+    time(Comprehension.normalize(API.bagSymbol)(_), "normalize")
+  } andThen {
+    Owner.at(Owner.enclosing)
+  }
+
+  // ---------------------------------------------------------------------------
+  // Spec data: a collection of (desugared, resugared) trees
+  // ---------------------------------------------------------------------------
+
+  // hard coded: 2 generators
+  val (inp1, exp1) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val names = flatten[(User, Click), DataBag] {
+        comprehension[DataBag[(User, Click)], DataBag] {
+          val u = generator(users$1)
+          head {
+            val clicks$1 = clicks
+            val map$1 = comprehension[(User, Click), DataBag] {
+              val c = generator(clicks$1)
+              head {
+                (u, c)
+              }
+            }
+            map$1
+          }
+        }
+      }
+      names
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val clicks$1 = clicks
+      val names = comprehension[(User, Click), DataBag] {
+        val u = generator(users$1)
+        val c = generator(clicks$1)
+        head {
+          (u, c)
+        }
+      }
+      names
+    })
+
+    (inp, exp)
+  }
+
+  // hard coded: 3 generators
+  val (inp2, exp2) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val names = flatten[(Ad, User, Click), DataBag] {
+        comprehension[DataBag[(Ad, User, Click)], DataBag] {
+          val u = generator(users$1)
+          head {
+            val clicks$1 = clicks
+            val flatMap$1 = flatten[(Ad, User, Click), DataBag] {
+              comprehension[DataBag[(Ad, User, Click)], DataBag] {
+                val c = generator(clicks$1)
+                head {
+                  val ads$1 = ads
+                  val map$1 = comprehension[(Ad, User, Click), DataBag] {
+                    val a = generator(ads$1)
+                    head {
+                      (a, u, c)
+                    }
+                  }
+                  map$1
+                }
+              }
+            }
+            flatMap$1
+          }
+        }
+      }
+      names
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val clicks$1 = clicks
+      val ads$1 = ads
+      val names = comprehension[(Ad, User, Click), DataBag] {
+        val u = generator(users$1)
+        val c = generator(clicks$1)
+        val a = generator(ads$1)
+        head {
+          (a, u, c)
+        }
+      }
+      names
+    })
+
+    (inp, exp)
+  }
+
+  // hard-coded: 3 generators and a filter
+  val (inp3, exp3) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val names = flatten[(Ad, User, Click), DataBag] {
+        comprehension[DataBag[(Ad, User, Click)], DataBag] {
+          val u = generator(users$1)
+          head {
+            val clicks$1 = clicks
+            val withFilter$1 = comprehension[Click, DataBag] {
+              val c = generator(clicks$1)
+              guard {
+                val x$1 = c.userID
+                val x$2 = u.id
+                val x$3 = x$1 == x$2
+                x$3
+              }
+              head(c)
+            }
+            val flatMap$1 = flatten[(Ad, User, Click), DataBag] {
+              comprehension[DataBag[(Ad, User, Click)], DataBag] {
+                val c = generator(withFilter$1)
+                head {
+                  val ads$1 = ads
+                  val map$1 = comprehension[(Ad, User, Click), DataBag] {
+                    val a = generator(ads$1)
+                    head {
+                      (a, u, c)
+                    }
+                  }
+                  map$1
+                }
+              }
+            }
+            flatMap$1
+          }
+        }
+      }
+      names
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val clicks$1 = clicks
+      val ads$1 = ads
+      val names = comprehension[(Ad, User, Click), DataBag] {
+        val u = generator(users$1)
+        val c = generator(clicks$1)
+        guard {
+          val x$1 = c.userID
+          val x$2 = u.id
+          val x$3 = x$1 == x$2
+          x$3
+        }
+        val a = generator(ads$1)
+        head {
+          (a, u, c)
+        }
+      }
+      names
+    })
+
+    (inp, exp)
+  }
+
+  // resugared: three generators and two filters at the end
+  val (inp4, exp4) = {
+
+    val inp = typeCheckAndANF(reify {
+      for {
+        u <- users
+        a <- ads
+        c <- clicks
+        if u.id == c.userID
+        if a.id == c.adID
+      } yield (c.time, a.`class`)
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val users$1 = users
+      val ads$1 = ads
+      val clicks$1 = clicks
+      comprehension[(Instant, AdClass.Value), DataBag] {
+        val u = generator(users$1)
+        val a = generator(ads$1)
+        val c = generator(clicks$1)
+        guard(u.id == c.userID)
+        guard(a.id == c.adID)
+        head(c.time, a.`class`)
+      }
+    })
+
+    (inp, exp)
+  }
+
+  // resugared: three generators and two interleaved filters
+  val (inp5, exp5) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      for {
+        c <- clicks
+        u <- users
+        if u.id == c.userID
+        a <- ads
+        if a.id == c.adID
+      } yield (c.time, a.`class`)
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val clicks$1 = clicks
+      val users$1 = users
+      val ads$1 = ads
+      comprehension[(Instant, AdClass.Value), DataBag] {
+        val c = generator(clicks$1)
+        val u = generator(users$1)
+        guard(u.id == c.userID)
+        val a = generator(ads$1)
+        guard(a.id == c.adID)
+        head(c.time, a.`class`)
+      }
+    })
+
+    (inp, exp)
+  }
+
+  // resugared: one patmat generator and no filters
+  val (inp6, exp6) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      for {
+        Click(adID, userID, time) <- clicks
+      } yield (adID, time)
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val clicks$11: DataBag[Click] = clicks;
+      comprehension[(Long, Instant), DataBag]({
+        val check$ifrefutable$1: Click = generator[Click, DataBag](clicks$11);
+        head[(Long, Instant)]({
+          val x$2: Click@unchecked = check$ifrefutable$1: Click@unchecked;
+          val adID$6: Long = x$2.adID;
+          val time$6: Instant = x$2.time;
+          Tuple2.apply[Long, Instant](adID$6, time$6)
+        })
+      })
+    })
+
+    (inp, exp)
+  }
+
+  // resugared: patmat with two generators and one filter
+  val (inp7, exp7) = {
+
+    val inp = (typeCheckAndANF andThen fix) (reify {
+      for {
+        Click(adID, userID, time) <- clicks
+        Ad(id, name, _) <- ads
+        if id == adID
+      } yield (adID, time)
+    })
+
+    val exp = (typeCheckAndANF andThen fix) (reify {
+      val clicks$1 = clicks;
+      val ads$1 = ads;
+      comprehension[(Long, Instant), DataBag]({
+        val click$1 = generator[Click, DataBag]({
+          clicks$1
+        });
+        val ad$1 = generator[Ad, DataBag]({
+          ads$1
+        });
+        guard({
+          val click$2 = click$1: Click@unchecked
+          val adID$1 = click$2.adID
+          val time$1 = click$2.time
+          val x$5: Ad = ad$1: Ad@unchecked;
+          val ad$2 = ad$1: Ad
+          val id$1 = ad$2.id
+          id$1 == adID$1
+        });
+        head[(Long, Instant)]({
+          val click$3 = click$1: Click@unchecked
+          val adID$2 = click$3.adID;
+          val time$2 = click$3.time;
+          Tuple2.apply[Long, Instant](adID$2, time$2)
+        })
+      })
+    })
+
+    (inp, exp)
+  }
+
+  // ---------------------------------------------------------------------------
+  // Spec tests
+  // ---------------------------------------------------------------------------
+
+  "normalize hard-coded comprehensions" - {
+    "with two generators" in {
+      normalize(inp1) shouldEqual exp1
+    }
+    "with three generators" in {
+      normalize(inp2) shouldEqual exp2
+    }
+    "with three generators and two filters" in {
+      normalize(inp3) shouldEqual exp3
+    }
+  }
+
+  "normalize resugared comprehensions" - {
+    "with three generators and two filters at the end" in {
+      normalize(inp4) shouldEqual exp4
+    }
+    "with three generators and two interleaved filters" in {
+      normalize(inp5) shouldEqual exp5
+    }
+    "with one patmat generator and no filters" in {
+      normalize(inp6) shouldEqual exp6
+    }
+    "with two patmat generators and one filter" in {
+      normalize(inp7) shouldEqual exp7
+    }
+  }
+}
