80665bf1c44e2fb29f6ba7010920974d7b0b446d
Georgi Krastev <joro.kr.21@gmail.com>
2016-04-25 13:00:01 +0200

    [Macros] Removed vars from ConvertersMacros

--- emma-common-macros/src/main/scala/eu/stratosphere/emma/macros/ConvertersMacros.scala
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/macros/ConvertersMacros.scala
@@ -5,7 +5,7 @@ import emma.compiler
 
 import scala.reflect.macros.blackbox
 
-/** Automatic derivation of CSV encoding / decoding for primitives and [[Product]] types. */
+/** Automatic derivation of CSV encoding / decoding for primitives and [[scala.Product]] types. */
 // TODO: add DateTime support
 class ConvertersMacros(val c: blackbox.Context) extends compiler.MacroUtil {
 
@@ -16,56 +16,49 @@ class ConvertersMacros(val c: blackbox.Context) extends compiler.MacroUtil {
   import Type._
   import Term.name.fresh
 
-  private val builder =
-    fresh("builder")
+  private val iter = fresh("iter")
+  private val const = q"$Scala.Function.const"
+  private val seq = q"$Scala.Seq"
 
   def materializeCSVConverters[T: c.WeakTypeTag] = Type.check {
     val T = Type.weak[T]
     val value = fresh("value")
-    val index = fresh("index")
     val sep = fresh("sep")
 
-    val from = q"""
-      ($value: ${array(string)}) => {
-        var $index = -1
-        ${fromCSV(T, q"$value", q"{ $index += 1; $index }")}
-      }"""
-
-    val to = q"""
-      ($value: $T, $sep: $char) => {
-        $builder.clear()
-        ${toCSV(T, q"$value")}
-        $builder.result()
-      }"""
-
-    q"""{
-      val $builder = $Scala.collection.mutable.ArrayBuilder.make[$string]
-      $emma.api.CSVConverters[$T]($from)($to)
+    val from = q"""($value: ${array(string)}) => {
+      val $iter = $value.iterator
+      ${fromCSV(T, q"$iter.next")}
     }"""
+
+    val to = q"""($value: $T, $sep: $char) =>
+      ${toCSV(T, q"$value")}.toArray
+    """
+
+    q"""$emma.api.CSVConverters[$T]($from)($to)"""
   }
 
-  def fromCSV(T: Type, value: Tree, index: Tree): Tree =
+  def fromCSV(T: Type, value: Tree): Tree =
     if (T <:< Type[Product]) {
       val method = T.decl(Term.name.init).alternatives.head.asMethod
       val params = method.typeSignatureIn(T).paramLists.head
-      val args = for (p <- params) yield fromCSV(Type.of(p), value, index)
+      val args = for (p <- params) yield fromCSV(Type of p, value)
       q"new $T(..$args)"
     } else {
       if (T =:= Type.unit || T =:= void) Term.unit
-      else if (T =:= bool || T =:= jBool) q"$value($index).toBoolean"
-      else if (T =:= char || T =:= jChar) q"$value($index).head"
-      else if (T =:= byte || T =:= jByte) q"$value($index).toByte"
-      else if (T =:= short || T =:= jShort) q"$value($index).toShort"
-      else if (T =:= int || T =:= jInt) q"$value($index).toInt"
-      else if (T =:= long || T =:= jLong) q"$value($index).toLong"
-      else if (T =:= float || T =:= jFloat) q"$value($index).toFloat"
-      else if (T =:= double || T =:= jDouble) q"$value($index).toDouble"
-      else if (T =:= bigInt) q"$Scala.BigInt($value($index))"
-      else if (T =:= bigDec) q"$Scala.BigDecimal($value($index))"
-      else if (T =:= jBigInt) q"new $Java.math.BigInteger($value($index))"
-      else if (T =:= jBigDec) q"new $Java.math.BigDecimal($value($index))"
-      else if (T =:= string) q"$value($index)"
-      else q"{ $index; ${null_(T)} }"
+      else if (T =:= bool || T =:= jBool) q"$value.toBoolean"
+      else if (T =:= char || T =:= jChar) q"$value.head"
+      else if (T =:= byte || T =:= jByte) q"$value.toByte"
+      else if (T =:= short || T =:= jShort) q"$value.toShort"
+      else if (T =:= int || T =:= jInt) q"$value.toInt"
+      else if (T =:= long || T =:= jLong) q"$value.toLong"
+      else if (T =:= float || T =:= jFloat) q"$value.toFloat"
+      else if (T =:= double || T =:= jDouble) q"$value.toDouble"
+      else if (T =:= bigInt) q"$Scala.BigInt($value)"
+      else if (T =:= bigDec) q"$Scala.BigDecimal($value)"
+      else if (T =:= jBigInt) q"new $Java.math.BigInteger($value)"
+      else if (T =:= jBigDec) q"new $Java.math.BigDecimal($value)"
+      else if (T =:= string) q"$value"
+      else q"$const(${null_(T)})($value)"
     }
 
   def toCSV(T: Type, value: Tree): Tree = {
@@ -80,9 +73,9 @@ class ConvertersMacros(val c: blackbox.Context) extends compiler.MacroUtil {
         if method.toString == param.toString
       } yield toCSV(result(method.infoIn(T)), q"$value.$method")
 
-      q"..$fields"
+      q"$seq(..$fields).flatten"
     } else {
-      q"$builder += $value.toString"
+      q"$seq($value.toString)"
     }
   }
 }
