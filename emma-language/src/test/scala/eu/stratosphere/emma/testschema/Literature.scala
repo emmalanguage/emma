22c669caf4966fcb3d8d2bb85ca1cec47cc9c318
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-05-04 10:47:02 +0200

    [IR] [TEST] Added Literature testschema.

---
+++ emma-language/src/test/scala/eu/stratosphere/emma/testschema/Literature.scala
@@ -0,0 +1,19 @@
+package eu.stratosphere.emma.testschema
+
+object Literature {
+
+  case class Book(title: String, author: String)
+
+  case class Character(name: String, book: Book)
+
+  // ---------------------------------------------------------------------------
+  // The Hitchhiker's Guide to the Galaxy
+  // ---------------------------------------------------------------------------
+
+  val hhBook = Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams")
+
+  val hhCrts = Seq(
+    Character("Arthur Dent", hhBook),
+    Character("Zaphod Beeblebrox", hhBook),
+    Character("Prostetnic Vogon Jeltz", hhBook))
+}
