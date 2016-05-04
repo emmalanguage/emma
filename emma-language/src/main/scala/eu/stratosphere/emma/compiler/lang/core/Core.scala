386c81c2d123bb260c574ce1a45e4df7daebf4aa
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-05-04 20:37:03 +0200

    [IR] `Core.Language` specification.

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/core/Core.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/core/Core.scala
@@ -17,6 +17,243 @@ trait Core extends Common
   object Core {
 
     // -------------------------------------------------------------------------
+    // Language
+    // -------------------------------------------------------------------------
+
+    /**
+     * The grammar associated with the [[Language]] objects and accepted by the
+     * [[Core.validate()]] method is as follows.
+     *
+     * {{{
+     * Atomic  = Null
+     *         | Lit[A](v: A)
+     *         | Pkg(sym: TypeSymbol)
+     *         | This(sym: TermSymbol)
+     *         | Ref(sym: TermSymbol)
+     *
+     * Term    = Atomic
+     *         | Sel(target: Atomic, member: TermSymbol)
+     *         | App(target: Atomic, targs: Seq[Type], argss: Seq[Seq[Atomic]])
+     *         | Call(target: Atomic, method: TermSymbol, targs: Seq[Type], argss: Seq[Seq[Atomic]])
+     *         | Inst(target: TypeSymbol, targs: Seq[Type], argss: Seq[Seq[Atomic]])
+     *         | Lambda(args: Seq[TermSymbol], body: Let)
+     *         | Typed(expr: Atomic, type: Type)
+     *
+     * Term'   = Term
+     *         | If(cond: Atomic, thn: Branch, els: Branch)
+     *
+     * Branch  = Atomic
+     *         | App
+     *
+     * Val     = (lhs: TermSymbol, rhs: Term)
+     *
+     * Let     = Let(vals: Seq[Val], defs: Seq[Def], expr: Term')
+     *
+     * Def     = Def(sym: MethodSymbol, params: TermSymbol, body: Let)
+     * }}}
+     */
+    object Language {
+      //@formatter:off
+
+      // -----------------------------------------------------------------------
+      // atomics
+      // -----------------------------------------------------------------------
+
+      val lit     = Term.lit            // literals
+      val ref     = Term.ref            // references
+
+      // -----------------------------------------------------------------------
+      // terms
+      // -----------------------------------------------------------------------
+
+      // selections
+      object sel {
+        def apply(target: Tree, member: TermSymbol, tpe: Type = NoType): Select = {
+          assert(isAtomic(target), "Selection target is not atomic")
+          Term.sel.apply(target, member, tpe)
+        }
+
+        def unapply(sel: Select): Option[(Tree, TermSymbol)] =
+          Term.sel.unapply(sel)
+      }
+
+      // function applications
+      object app {
+        def apply(target: Tree, targs: Type*)(argss: Seq[Tree]*): Apply = {
+          assert(isAtomic(target), "Application target is not atomic")
+          assert(argss forall (_ forall isAtomic), "Not all application args are atomic")
+          Term.app.apply(target, targs: _*)(argss: _*)
+        }
+
+        def unapplySeq(apply: Apply): Option[(Tree, Seq[Type], Seq[Seq[Tree]])] = apply match {
+          case Term.app(fun, targs, argss@_*) => fun match {
+            case ref(_) => Some(fun, targs, argss)
+            case lit(_) => Some(fun, targs, argss)
+            case _ => None
+          }
+          case _ => None
+        }
+      }
+
+      // method calls
+      object call {
+        def apply(target: Tree, method: TermSymbol, targs: Type*)(argss: Seq[Tree]*): Tree = {
+          assert(isAtomic(target), "Method call target is not atomic or sel")
+          assert(argss forall (_ forall isAtomic), "Not all method call args are atomic")
+          Method.call.apply(target, method, targs: _*)(argss: _*)
+        }
+
+        def unapplySeq(tree: Tree): Option[(Tree, TermSymbol, Seq[Type], Seq[Seq[Tree]])] =
+          Method.call.unapplySeq(tree)
+      }
+
+      // class instantiations
+      object inst {
+        def apply(target: TypeSymbol, types: Type*)(argss: Seq[Tree]*): Tree = {
+          assert(argss forall (_ forall isAtomic), "Not all instantiation args are atomic")
+          Term.inst.apply(target, types: _*)(argss: _*)
+        }
+
+        def unapplySeq(tree: Tree): Option[(TypeSymbol, Seq[Type], Seq[Seq[Tree]])] =
+          Term.inst.unapplySeq(tree)
+      }
+
+      // lambdas
+      object lambda {
+        def apply(args: TermSymbol*)(body: Block): Function = {
+          assert(isLetExpr(body), "Method body is not a let expression")
+          Term.lambda.apply(args: _*)(body)
+        }
+
+        def unapply(fun: Function): Option[(TermSymbol, Seq[TermSymbol], Block)] = fun match {
+          case Term.lambda(sym, args, body: Block) => Some(sym, args, body)
+          case _ => None
+        }
+      }
+
+      // type ascriptions
+      object typed {
+        def apply(tree: Tree, tpe: Type): Typed = {
+          assert(isAtomic(tree), "Selection target is not atomic")
+          Type.ascription.apply(tree, tpe)
+        }
+
+        def unapply(tpd: Typed): Option[(Tree, Type)] =
+          Type.ascription.unapply(tpd)
+      }
+
+      // -----------------------------------------------------------------------
+      // let expressions
+      // -----------------------------------------------------------------------
+
+      // val definitions
+      object val_ {
+        def apply(lhs: TermSymbol, rhs: Tree = EmptyTree, flags: FlagSet = Flag.SYNTHETIC): ValDef = {
+          assert(!isMutable(flags), "ValDef is not immutable")
+          Tree.val_.apply(lhs, rhs, flags)
+        }
+
+        def unapply(value: ValDef): Option[(TermSymbol, Tree, FlagSet)] = value match {
+          case Tree.val_(lhs, rhs, flags) if !isMutable(flags) => Some(lhs, rhs, flags)
+          case _ => None
+        }
+
+        def isMutable(flags: FlagSet): Boolean =
+          (flags | Flag.MUTABLE) == flags
+      }
+
+      // let expressions
+      object let_ {
+        def apply(vals: Seq[ValDef], defs: Seq[DefDef], expr: Tree): Block = {
+          assert(isTerm(expr) || isBranch(expr), "Let expr head is not a term or a branch")
+          Tree.block.apply(vals ++ defs :+ expr)
+        }
+
+        def unapply(block: Block): Option[(List[ValDef], List[DefDef], Tree)] = block match {
+          case Tree.block(stats, expr) =>
+            val vals = List.newBuilder[ValDef]
+            val defs = List.newBuilder[DefDef]
+
+            def expectAll(list: List[Tree]): List[Tree] = list match {
+              case (t: ValDef) :: rest => vals += t; expectAll(rest)
+              case (t: DefDef) :: rest => defs += t; expectDef(rest)
+              case _ => list
+            }
+
+            def expectDef(list: List[Tree]): List[Tree] = list match {
+              case (t: DefDef) :: rest => defs += t; expectDef(rest)
+              case _ => list
+            }
+
+            if (expectAll(stats).isEmpty) Some(vals.result(), defs.result(), expr)
+            else None
+        }
+      }
+
+      // -----------------------------------------------------------------------
+      // control flow
+      // -----------------------------------------------------------------------
+
+      // conditionals
+      object if_ {
+        def apply(cond: Tree, thn: Tree, els: Tree): If = {
+          assert(isAtomic(cond), "If condition is not atomic")
+          assert(isAtomic(cond) || isApp(thn), "If then clause is not atomic or function application")
+          assert(isAtomic(cond) || isApp(els), "If else clause is not atomic or function application")
+          Tree.branch.apply(cond, thn, els)
+        }
+
+        def unapply(branch: If): Option[(Tree, Tree, Tree)] =
+          Tree.branch.unapply(branch)
+      }
+
+      // simple method definitions
+      object def_ {
+        def apply(sym: MethodSymbol, flags: FlagSet = Flag.SYNTHETIC)(params: TermSymbol*)(body: Block): DefDef = {
+          assert(isLetExpr(body), "Method body is not a let expression")
+          Method.def_.apply(sym, flags)(params: _*)(body)
+        }
+
+        def unapply(dd: DefDef): Option[(MethodSymbol, FlagSet, List[TermSymbol], Block)] =
+          Method.def_.unapply(dd)
+      }
+
+      //@formatter:on
+
+      private def isAtomic(tree: Tree): Boolean = tree match {
+        case ref(_) => true
+        case lit(_) => true
+        // case pkg(_) => true // TODO
+        case _ => false
+      }
+
+      private def isTerm(tree: Tree): Boolean = isAtomic(tree) || (tree match {
+        case sel(_, _) => true
+        case app(_, _, _*) => true
+        case call(_, _, _, _*) => true
+        case inst(_, _, _*) => true
+        case lambda(_, _, _) => true
+        case typed(_, _) => true
+        case _ => false
+      })
+
+      private def isApp(tree: Tree): Boolean = tree match {
+        case app(_, _, _*) => true
+        case _ => false
+      }
+
+      private def isBranch(tree: Tree): Boolean = tree match {
+        case if_(_, _, _) => true
+        case _ => false
+      }
+
+      private def isLetExpr(tree: Tree): Boolean = tree match {
+        case let_(_, _, _) => true
+        case _ => false
+      }
+    }
+
+    // -------------------------------------------------------------------------
     // Validate API
     // -------------------------------------------------------------------------
 
--- emma-language/src/test/scala/eu/stratosphere/emma/compiler/lang/core/LanguageSpec.scala
+++ emma-language/src/test/scala/eu/stratosphere/emma/compiler/lang/core/LanguageSpec.scala
@@ -1,7 +1,9 @@
 package eu.stratosphere.emma.compiler.lang.core
 
-import eu.stratosphere.emma.api.{CSVInputFormat, DataBag}
+import eu.stratosphere.emma.api.DataBag
 import eu.stratosphere.emma.compiler.BaseCompilerSpec
+import eu.stratosphere.emma.testschema.Marketing._
+import org.example.foo.{Bar, Baz}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
@@ -11,6 +13,7 @@ class LanguageSpec extends BaseCompilerSpec {
 
   import compiler._
   import universe._
+  import Core.Language._
 
   def typeCheckAndValidate[T]: Expr[T] => Boolean = {
     (_: Expr[T]).tree
@@ -20,168 +23,465 @@ class LanguageSpec extends BaseCompilerSpec {
     time(Core.validate(_), "validate")
   }
 
-  // modeled by `Literal(Constant(value))` nodes
-  "literals" in {
-    typeCheckAndValidate(reify {
-      42
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      4.2
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      "42"
-    }) shouldBe true
+  def validate: Tree => Boolean = {
+    time(Core.validate(_), "validate")
   }
 
-  // modeled by `Ident(name)` where `sym` is a "free term symbol"
-  "identifiers" in {
+  val repair: Tree => Tree =
+    Owner.at(Owner.enclosing)
+
+  // ---------------------------------------------------------------------------
+  // atomics
+  // ---------------------------------------------------------------------------
+
+  "literals" - {
+    // modeled by
+    // - `lit` objects in `Core.Language`
+    // - `Literal(Constant(value))` nodes in Scala ASTs
+
+    val examples = List(
+      typeCheck(reify(42)),
+      typeCheck(reify(4.2)),
+      typeCheck(reify("42")))
+      .map(_ find {
+        case lit(_) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be destructed and constructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case lit(const) =>
+          repair(lit(const)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "references" - {
+    // modeled by
+    // - `ref` objects in `Core.Language`
+    // - `Ident(name)` nodes where `sym` is a (free) TermSymbol in Scala ASTs
+
     val u = 42
-    typeCheckAndValidate(reify {
-      u
-    }) shouldBe true
-  }
+    val v = 4.2
+    val w = "42"
 
-  // modeled by `Typed(expr, tpt)` nodes
-  "type ascriptions" in {
-    typeCheckAndValidate(reify {
-      42: Int
-    }) shouldBe true
-  }
+    val examples = List(
+      typeCheck(reify(u)),
+      typeCheck(reify(v)),
+      typeCheck(reify(w)))
+      .map(_ find {
+        case ref(_) => true
+        case _ => false
+      })
 
-  // modeled by `Select(qualifier, name)` nodes
-  "field selections" in {
-    typeCheckAndValidate(reify {
-      t._2
-    }) shouldBe true
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
 
-    typeCheckAndValidate(reify {
-      scala.Int.MaxValue
-    }) shouldBe true
-  }
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
 
-  // modeled by `Block(stats, expr)` nodes
-  "blocks" in {
-    typeCheckAndValidate(reify {
-      val z = 5
-      x + z
-    }) shouldBe true
-  }
-
-  // modeled by `ValDef(lhs, rhs)` nodes
-  "value and variable definitions" in {
-    typeCheckAndValidate(reify {
-      val u = 42
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      val u = s"$x is $y"
-    }) shouldBe true
-  }
-
-  // modeled by `Function(args, body)` nodes
-  "anonymous function definitions" - {
-    typeCheckAndValidate(reify {
-      (x: Int, y: Int) => x + y
-    }) shouldBe true
-  }
-
-  // modeled by `TypeApply(fun, args)` nodes
-  "type applications" in {
-    typeCheckAndValidate(reify {
-      Seq.empty[Int]
-    }) shouldBe true
-  }
-
-  // modeled by `Apply(fun, args)` nodes
-  "function applications" in {
-    typeCheckAndValidate(reify {
-      x == 42
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      x + 4.2
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      scala.Predef.println(y)
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      y.substring(1)
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      ((x: Int, y: Int) => x + y) (x, x)
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      Seq(x, x)
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      val zs: DataBag[Int] = DataBag(xs.fetch())
-    }) shouldBe true
-  }
-
-  // modeled by `New` nodes
-  "class instantiation" in {
-    typeCheckAndValidate(reify {
-      new Tuple2("route", 66)
-    }) shouldBe true
-
-    typeCheckAndValidate(reify {
-      new CSVInputFormat[(Int, String)]
-    }) shouldBe true
-  }
-
-  // modeled by `If(cond, thenp, elsep)` nodes
-  "conditionals" in {
-    typeCheckAndValidate(reify {
-      if (x == 42) x else x / 42
-    }) shouldBe true
-  }
-
-  // modeled direct-style by `DefDef` call chains
-  "while loops" in {
-    typeCheckAndValidate(reify {
-      def b$00(): Unit = {
-        val i$1 = 0
-        val r$1 = 0
-        def b$01$while(i: Int, r: Int): Int = {
-          def b$01$body(i: Int, r: Int): Int = {
-            val i$2 = i + 1
-            val r$2 = r * i$2
-            b$01$while(i$2, r$2)
-          }
-          def b$02(i: Int, r: Int): Int = {
-            i * r
-          }
-          if (i < x) b$01$body(i, r)
-          else b$02(i, r)
-        }
-        b$01$while(i$1, r$1)
+    "can be destructed and constructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case ref(sym) =>
+          repair(ref(sym)) shouldBe alphaEqTo(example)
       }
-      b$00()
-    }) shouldBe true
+    }
+  }
 
-    typeCheckAndValidate(reify {
-      def b$00(): Unit = {
-        val i$1 = 0
-        val r$1 = 0
-        def b$01$dowhile(i: Int, r: Int): Int = {
-          val i$2 = i + 1
-          val r$2 = r * i$2
-          def b$02(i: Int, r: Int): Int = {
-            i * r
-          }
-          if (i < x) b$01$dowhile(i$2, r$2)
-          else b$02(i, r)
-        }
-        b$01$dowhile(i$1, r$1)
+  // ---------------------------------------------------------------------------
+  // terms
+  // ---------------------------------------------------------------------------
+
+  "selections" - {
+    // modeled by
+    // - `sel` objects in `Core.Language`
+    // - `Select(qualifier, name)` nodes in Scala ASTs
+
+    val t = this.t
+
+    val examples = List(
+      typeCheck(reify(t._2)),
+      typeCheck(reify(DEFAULT_CLASS)))
+      .map(_ find {
+        case sel(_, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be destructed and constructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case sel(tgt, member) =>
+          repair(sel(tgt, member)) shouldBe alphaEqTo(example)
       }
-      b$00()
-    }) shouldBe true
+    }
+  }
+
+  "function applications" - {
+    // modeled by
+    // - `app` objects in `Core.Language`
+    // - `Apply(fun, args)` nodes in Scala ASTs
+
+    val x = this.x
+
+    val examples = List(
+      typeCheck(reify {
+        def max(x: Int, y: Int): Int = Math.max(x, y)
+        max(42, x)
+      }))
+      .map(_ find {
+        case app(_, _, _*) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be destructed and constructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case app(fn, targs, argss@_*) =>
+          repair(app(fn, targs: _*)(argss: _*)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "method calls" - {
+    // modeled by
+    // - `call` objects in `Core.Language`
+    // - `Apply(Select(id, TermSymbol), args)` nodes in Scala ASTs
+
+    val x = this.x
+    val y = this.y
+    val xs = this.xs
+    val ys = this.ys.fetch()
+
+    val examples = List(
+      typeCheck(reify {
+        x == 42
+      }),
+      typeCheck(reify {
+        scala.Predef.println(y)
+      }),
+      typeCheck(reify {
+        y.substring(1)
+      }),
+      typeCheck(reify {
+        val f = (x: Int, y: Int) => Math.max(x, y)
+        f(42, x)
+      }),
+      typeCheck(reify {
+        Seq(x, x)
+      }),
+      typeCheck(reify {
+        xs.fetch()
+      }),
+      typeCheck(reify {
+        DataBag(ys)
+      }))
+      .map(_ find {
+        case call(_, _, _, _*) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be destructed and constructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case call(target, method, targs, argss@_*) =>
+          repair(repair(call(target, method, targs: _*)(argss: _*))) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "class instantiations" - {
+    // modeled by
+    // - `inst` objects in `Core.Language`
+    // - `Apply(tpt: New, _)` nodes in Scala ASTs
+
+    val x = this.x
+    val adClass = AdClass.SERVICES
+    implicit val intNumeric = Numeric.IntIsIntegral
+
+    val examples = List(
+      typeCheck(reify(new Ad(1, "Uber AD", adClass))),
+      typeCheck(reify(new Baz(x))),
+      typeCheck(reify(new Bar[Int](x))))
+      .map(_ find {
+        case inst(_, _, _*) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case inst(clazz, targs, argss@_*) =>
+          repair(inst(clazz, targs: _*)(argss: _*)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "lambdas" - {
+    // modeled by
+    // - `lambda` objects in `Core.Language`
+    // - `Function(args, body)` nodes nodes in Scala ASTs
+
+    val examples = List(
+      typeCheck(reify((x: Int, y: Int) => { val s = x + y; s * 15 })))
+      .map(_ find {
+        case lambda(_, _, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    assert(examples.nonEmpty, "No spec examples given")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case lambda(sym, params, body) =>
+          repair(lambda(params: _*)(body)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "type ascriptions" - {
+    // modeled by
+    // - `inst` objects in `Core.Language`
+    // - `Typed(expr, tpt)` nodes nodes in Scala ASTs
+
+    val x = this.x
+    val t = this.t
+
+    val examples = List(
+      typeCheck(reify(x: Int)),
+      typeCheck(reify(t: (Any, String))))
+      .map(_ find {
+        case typed(_, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case typed(tree, tpe) =>
+          repair(typed(tree, tpe)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+  // let expressions
+  // ---------------------------------------------------------------------------
+
+  "value and variable definitions" - {
+    // modeled by
+    // - `val_` objects in `Core.Language`
+    // - `ValDef(lhs, rhs)` nodes nodes in Scala ASTs
+
+    val x = this.x
+    val y = this.y
+
+    val examples = List(
+      typeCheck(reify {
+        val u = s"$x is $y"
+      }),
+      typeCheck(reify {
+        val u = 42
+      }))
+      .map(_ find {
+        case val_(_, _, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case val_(lhs, rhs, flags) =>
+          repair(val_(lhs, rhs, flags)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+  
+  "let expressions" - {
+    // modeled by
+    // - `let_` objects in `Core.Language`
+    // - `Block(stats, expr)` nodes nodes in Scala ASTs
+
+    val x = this.x
+    val y = this.y
+
+    val examples = List(
+      typeCheck(reify {
+        val u = s"$x is $y"
+        val z = u(15)
+        z == 'x'
+      }),
+      typeCheck(reify {
+        val q = "The meaning of life, the universe and everything?"
+        val a = 42
+        (q, a)
+      }),
+      typeCheck(reify {
+        val q = "Factorial of the answer for the meaning of life, the universe and everything?"
+        val a = 42
+        def fac(n: Int): Int =
+          if (n > 0) n * fac(n-1)
+          else 1
+        fac(a)
+      }))
+      .map(_ find {
+        case let_(_, _, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case let_(vals, defs, expr) =>
+          repair(let_(vals, defs, expr)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  // ---------------------------------------------------------------------------
+  // control flow
+  // ---------------------------------------------------------------------------
+
+  "conditionals" - {
+    // modeled by
+    // - `block` objects in `Source.Language`
+    // - `If(cond, thenp, elsep)` nodes nodes in Scala ASTs
+
+    val expCnd = this.x == 42
+    val expThn = this.x
+    val expEls = this.x - 42
+
+    val examples = List(
+      typeCheck(reify(if (expCnd) expThn else expEls)))
+      .map(_ find {
+        case if_(_, _, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case if_(cond, thn, els) =>
+          repair(if_(cond, thn, els)) shouldBe alphaEqTo(example)
+      }
+    }
+  }
+
+  "simple functions" - {
+    // modeled by
+    // - `block` objects in `Source.Language`
+    // - `If(cond, thenp, elsep)` nodes nodes in Scala ASTs
+
+    val cnd = this.x == 42
+    val thn = this.x
+    val els = this.x - 42
+
+    val examples = List(
+      typeCheck(reify {
+        def max(x: Int, y: Int): Int = {
+          val m = Math.max(x, y)
+          Math.max(m, 0)
+        }
+      }),
+      typeCheck(reify {
+        def fac(n: Int): Int = {
+          val more = n > 0
+          if (more) n * fac(n-1)
+          else 1
+        }
+      }))
+      .map(_ find {
+        case def_(_, _, _, _) => true
+        case _ => false
+      })
+
+    assert(examples.nonEmpty, "No spec examples given")
+    assert(examples.forall(_.isDefined), "Not all code snippets define an expected match")
+
+    "are valid language constructs" in {
+      for (Some(example) <- examples)
+        validate(example) shouldBe true
+    }
+
+    "can be constructed and destructed" in {
+      for ((Some(example), i) <- examples.zipWithIndex) example match {
+        case def_(sym, flags, params, body) =>
+          repair(def_(sym, flags)(params: _*)(body)) shouldBe alphaEqTo(example)
+      }
+    }
   }
 }
