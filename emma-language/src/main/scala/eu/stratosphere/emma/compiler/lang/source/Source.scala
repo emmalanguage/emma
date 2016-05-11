3e422cf4cfeb82f5bdad8ef981ab8a3aa40431f7
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-05-11 16:44:05 +0200

    [IR] `Source.Language` improvements.
    - Formatted `Source.Language` element blocks.

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/source/Source.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/source/Source.scala
@@ -65,29 +65,44 @@ trait Source extends Common
     object Language {
       //@formatter:off
 
+      // -----------------------------------------------------------------------
       // atomics
+      // -----------------------------------------------------------------------
+
       val this_   = Term.this_          // this references
       val lit     = Term.lit            // literals
       val ref     = Term.ref            // references
 
+      // -----------------------------------------------------------------------
       // terms
+      // -----------------------------------------------------------------------
+
       val sel     = Term.sel            // selections
       val app     = Term.app            // function applications
       val inst    = Term.inst           // class instantiation
       val lambda  = Term.lambda         // lambdas
       val typed   = Type.ascription     // type ascriptions
 
+      // -----------------------------------------------------------------------
       // state
+      // -----------------------------------------------------------------------
+
       val val_    = Tree.val_           // val and var definitions
       val assign  = Tree.assign         // assignments
       val block   = Tree.block          // blocks
 
+      // -----------------------------------------------------------------------
       // control flow
+      // -----------------------------------------------------------------------
+
       val branch  = Tree.branch         // conditionals
       val whiledo = Tree.while_         // while loop
       val dowhile = Tree.doWhile        // do while loop
 
+      // -----------------------------------------------------------------------
       // pattern matching
+      // -----------------------------------------------------------------------
+
       val match_  = Tree.match_         // pattern match
       val case_   = Tree.case_          // case
       val bind    = Tree.bind           // bind
@@ -106,4 +121,5 @@ trait Source extends Common
     def validate(tree: Tree): Boolean =
       valid(tree).isGood
   }
+
 }
