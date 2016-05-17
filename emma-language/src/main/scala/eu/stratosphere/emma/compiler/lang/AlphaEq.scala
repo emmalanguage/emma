f6953b7862b0ec8b59948aa4b5aefb59c3bb9f96
Georgi Krastev <joro.kr.21@gmail.com>
2016-05-17 14:33:01 +0200

    [IR] Simplified AlphaEq and fixed minor issues
     - A typo that caused a lhs to be compared to itself instead of rhs in one case.
     - Better error messages for flags (before it would print the internal mask as a number).

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/AlphaEq.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/lang/AlphaEq.scala
@@ -43,280 +43,234 @@ trait AlphaEq extends Common {
 
     import scala.language.implicitConversions
 
-    /** implicitly convert `alphaEq _` to a tupled version. */
+    // implicitly convert `alphaEq _` to a tupled version
     implicit def tupled(f: (Tree, Tree) => Eq Or Neq): ((Tree, Tree)) => Eq Or Neq =
       f.tupled
 
-    /** Provides `failHere` as alias for `failHere`. */
-    lazy val failHere = fail.at(lhs, rhs)
+    // provides `failHere` as alias for `fail at (lhs, rhs)`
+    lazy val failHere = fail at (lhs, rhs)
 
     // an accumulator dictionary for alpha equivalences
-    implicit val dict = mutable.Map.empty[Symbol, Symbol]
+    val dict = mutable.Map.empty[Symbol, Symbol]
 
     // -------------------------------------------------------------------------
     // Helper functions
     // -------------------------------------------------------------------------
 
-    def eqTrees(lhs: Tree, rhs: Tree): Eq Or Neq = {
+    def trees(lhs: Tree, rhs: Tree): Eq Or Neq = (lhs, rhs) match {
+      case (EmptyTree, EmptyTree) =>
+        pass
 
-      (lhs, rhs) match {
+      case (
+        Annotated(annot$l, arg$l),
+        Annotated(annot$r, arg$r)
+        ) => for {
+          eq <- trees(annot$l, annot$r)
+          eq <- trees(arg$l, arg$r)
+        } yield eq
 
-        case (EmptyTree, EmptyTree) =>
-          pass
+      case (
+        This(qual$l),
+        This(qual$r)
+        ) => for {
+          eq <- symbols(lhs, lhs)
+          eq <- names(qual$l, qual$r)
+        } yield eq
 
-        case (
-          lhs@Annotated(anot$l, argt$l),
-          rhs@Annotated(anot$r, argt$r)
-          ) =>
-          for {
-            anotEq <- eqTrees(anot$l, anot$r)
-            argtEq <- eqTrees(argt$l, argt$r)
-          } yield pass
+      case (
+        lhs: TypeTree,
+        rhs: TypeTree
+        ) => for {
+          eq <- types(lhs, rhs)
+        } yield eq
 
-        case (
-          lhs@This(name$l),
-          rhs@This(name$r)
-          ) =>
-          for {
-            symbEq <- eqSymbs(lhs, lhs)
-            nameEq <- eqNames(name$l, name$r)(lhs, rhs)
-          } yield pass
+      case (
+        lhs: AppliedTypeTree,
+        rhs: AppliedTypeTree
+        ) => for {
+          eq <- types(lhs, rhs)
+        } yield eq
 
-        case (
-          lhs: TypeTree,
-          rhs: TypeTree
-          ) =>
-          for {
-            typeEq <- eqTypes(lhs, rhs)
-          } yield pass
+      case (
+        lhs: Literal,
+        rhs: Literal
+        ) => for {
+          eq <- literals(lhs, lhs)
+        } yield eq
 
-        case (
-          lhs: AppliedTypeTree,
-          rhs: AppliedTypeTree
-          ) =>
-          for {
-            typeEq <- eqTypes(lhs, rhs)
-          } yield pass
+      case (
+        lhs: Ident,
+        rhs: Ident
+        ) => for {
+          eq <- symbols(lhs, rhs)
+        } yield eq
 
-        case (
-          lhs: Literal,
-          rhs: Literal
-          ) =>
-          for {
-            typeEq <- eqLtrls(lhs, lhs)
-          } yield pass
+      case (
+        Typed(expr$l, tpt$l),
+        Typed(expr$r, tpt$r)
+        ) => for {
+          eq <- trees(expr$l, expr$r)
+          eq <- trees(tpt$l, tpt$r)
+        } yield eq
 
-        case (
-          lhs: Ident,
-          rhs: Ident
-          ) =>
-          for {
-            symbEq <- eqSymbs(lhs, rhs)
-          } yield pass
+      case (
+        Select(qual$l, member$l),
+        Select(qual$r, member$r)
+        ) => for {
+          eq <- trees(qual$l, qual$r)
+          eq <- names(member$l, member$r)
+        } yield eq
 
-        case (
-          lhs@Typed(expr$l, tpet$l),
-          rhs@Typed(expr$r, tpet$r)
-          ) =>
-          for {
-            exprEq <- eqTrees(expr$l, expr$l)
-            tpetEq <- eqTrees(tpet$l, tpet$r)
-          } yield pass
+      case (
+        Block(stats$l, expr$l),
+        Block(stats$r, expr$r)
+        ) => for {
+          eq <- size(stats$l, stats$r, "block statements")
+          eq <- all(lhs.children, rhs.children)
+        } yield eq
 
-        case (
-          lhs@Select(qual$l, name$l),
-          rhs@Select(qual$r, name$r)
-          ) =>
-          for {
-            qualEq <- eqTrees(qual$l, qual$r)
-            nameEq <- eqNames(name$l, name$r)(lhs, rhs)
-          } yield pass
+      case (
+        ValDef(mods$l, _, tpt$l, rhs$l),
+        ValDef(mods$r, _, tpt$r, rhs$r)
+        ) => for {
+          eq <- modifiers(mods$l, mods$r)
+          eq <- types(tpt$l, tpt$r)
+          _ = dict += lhs.symbol -> rhs.symbol
+          eq <- trees(rhs$l, rhs$r)
+        } yield eq
 
-        case (
-          lhs@Block(stats$l, expr$l),
-          rhs@Block(stats$r, expr$r)
-          ) =>
-          for {
-            sizeEq <- {
-              if (stats$l.size == stats$r.size) pass
-              else failHere because s"Number of block statements does not match"
-            }
-            statEq <- eqSeqns(lhs.children, rhs.children)
-          } yield pass
+      case (
+        Assign(lhs$l, rhs$l),
+        Assign(lhs$r, rhs$r)
+        ) => for {
+          eq <- trees(lhs$l, lhs$r)
+          eq <- trees(rhs$l, rhs$r)
+        } yield eq
 
-        case (
-          lhs@ValDef(mods$l, _, tpt$l, rhs$l),
-          rhs@ValDef(mods$r, _, tpt$r, rhs$r)
-          ) =>
-          dict += lhs.symbol -> rhs.symbol
-          for {
-            modsEq <- eqFlags(mods$l.flags, mods$r.flags)(lhs, rhs)
-            typeEq <- eqTypes(tpt$l, tpt$r)
-            exprEq <- eqTrees(rhs$l, rhs$r)
-          } yield pass
+      case (
+        Function(params$l, body$l),
+        Function(params$r, body$r)
+        ) => for {
+          eq <- size(params$l, params$r, "function parameters")
+          eq <- all(params$l, params$r)
+          _ = dict += lhs.symbol -> rhs.symbol
+          eq <- trees(body$l, body$r)
+        } yield eq
 
-        case (
-          lhs@Assign(lhs$l, rhs$l),
-          rhs@Assign(lhs$r, rhs$r)
-          ) =>
-          for {
-            lhdsEq <- eqTrees(lhs$l, lhs$r)
-            rhdsEq <- eqTrees(rhs$l, rhs$r)
-          } yield pass
+      case (
+        TypeApply(target$l, targs$l),
+        TypeApply(target$r, targs$r)
+        ) => for {
+          eq <- symbols(lhs, rhs)
+          eq <- trees(target$l, target$r)
+          eq <- size(targs$l, targs$r, "type arguments")
+          eq <- all(targs$l, targs$r)
+        } yield eq
 
-        case (
-          lhs@Function(params$l, body$l),
-          rhs@Function(params$r, body$r)
-          ) =>
-          dict += lhs.symbol -> rhs.symbol
-          for {
-            sizeEq <- {
-              if (params$l.size == params$r.size) pass
-              else failHere because s"Number of function parameters does not match"
-            }
-            parsEq <- eqSeqns(params$l, params$r)
-            bodyEq <- eqTrees(body$l, body$r)
-          } yield pass
+      case (
+        Apply(target$l, args$l),
+        Apply(target$r, args$r)
+        ) => for {
+          eq <- symbols(lhs, rhs)
+          eq <- trees(target$l, target$r)
+          eq <- size(args$l, args$r, "application arguments")
+          eq <- all(args$l, args$r)
+        } yield eq
 
-        case (
-          lhs@TypeApply(fun$l, types$l),
-          rhs@TypeApply(fun$r, types$r)
-          ) =>
-          for {
-            symbEq <- eqSymbs(lhs, rhs)
-            funcEq <- eqTrees(fun$l, fun$r)
-            sizeEq <- {
-              if (types$l.size == types$r.size) pass
-              else failHere because s"Number of type parameters does not match"
-            }
-            typeEq <- eqSeqns(types$l, types$r)
-          } yield pass
+      case (
+        New(tpt$l),
+        New(tpt$r)
+        ) => for {
+          eq <- types(tpt$l, tpt$r)
+        } yield eq
 
-        case (
-          lhs@Apply(fun$l, args$l),
-          rhs@Apply(fun$r, args$r)
-          ) =>
-          for {
-            symbEq <- eqSymbs(lhs, rhs)
-            funcEq <- eqTrees(fun$l, fun$r)
-            sizeEq <- {
-              if (args$l.size == args$r.size) pass
-              else failHere because s"Number of function arguments does not match"
-            }
-            argsEq <- eqSeqns(args$l, args$r)
-          } yield pass
+      case (
+        If(cond$l, then$l, else$l),
+        If(cond$r, then$r, else$r)
+        ) => for {
+          eq <- trees(cond$l, cond$r)
+          eq <- trees(then$l, then$r)
+          eq <- trees(else$l, else$r)
+        } yield eq
 
-        case (
-          lhs@New(tpt$l),
-          rhs@New(tpt$r)
-          ) =>
-          for {
-            typeEq <- eqTypes(tpt$l, tpt$r)
-          } yield pass
+      case (
+        while_(cond$l, body$l),
+        while_(cond$r, body$r)
+        ) => for {
+          eq <- trees(cond$l, cond$r)
+          eq <- trees(body$l, body$r)
+        } yield eq
 
-        case (
-          lhs@If(cond$l, then$l, else$l),
-          rhs@If(cond$r, then$r, else$r)
-          ) =>
-          for {
-            condEq <- eqTrees(cond$l, cond$r)
-            thenEq <- eqTrees(then$l, then$r)
-            elseEq <- eqTrees(else$l, else$r)
-          } yield pass
+      case (
+        doWhile(cond$l, body$l),
+        doWhile(cond$r, body$r)
+        ) => for {
+          eq <- trees(cond$l, cond$r)
+          eq <- trees(body$l, body$r)
+        } yield eq
 
-        case (
-          lhs@while_(cond$l, body$l),
-          rhs@while_(cond$r, body$r)
-          ) =>
-          for {
-            condEq <- eqTrees(cond$l, cond$r)
-            bodyEq <- eqTrees(body$l, body$r)
-          } yield pass
+      case (
+        DefDef(mods$l, _, tparams$l, paramss$l, tpt$l, rhs$l),
+        DefDef(mods$r, _, tparams$r, paramss$r, tpt$r, rhs$r)
+        ) => for {
+          eq <- modifiers(mods$l, mods$r)
+          eq <- trees(tpt$l, tpt$r)
+          eq <- size(tparams$l, tparams$r, "type parameters")
+          eq <- all(tparams$l, tparams$r)
+          eq <- shape(paramss$l, paramss$r, "method parameters")
+          eq <- all(paramss$l.flatten, paramss$r.flatten)
+          _ = dict += lhs.symbol -> rhs.symbol
+          eq <- trees(rhs$l, rhs$r)
+        } yield eq
 
-        case (
-          lhs@doWhile(cond$l, body$l),
-          rhs@doWhile(cond$r, body$r)
-          ) =>
-          for {
-            condEq <- eqTrees(cond$l, cond$r)
-            bodyEq <- eqTrees(body$l, body$r)
-          } yield pass
+      case (
+        Match(selector$l, cases$l),
+        Match(selector$r, cases$r)
+        ) => for {
+          eq <- trees(selector$l, selector$r)
+          eq <- size(cases$l, cases$r, "pattern cases")
+          eq <- all(cases$l, cases$r)
+        } yield eq
 
-        case (
-          lhs@DefDef(mods$l, _, tps$l, vpss$l, tpt$l, rhs$l),
-          rhs@DefDef(mods$r, _, tps$r, vpss$r, tpt$r, rhs$r)) =>
-          dict += lhs.symbol -> rhs.symbol
-          for {
-            modsEq <- eqFlags(mods$l.flags, mods$r.flags)(lhs, rhs)
-            tptsEq <- eqTrees(tpt$l, tpt$r)
-            tsizEq <- {
-              if (tps$l.size == tps$r.size) pass
-              else failHere because s"Number of types does not match"
-            }
-            typeEq <- eqSeqns(tps$l, tps$r)
-            psizEq <- {
-              lazy val outerSizeEq = vpss$l.size == vpss$r.size
-              lazy val innerSizeEq = (vpss$l zip vpss$r) forall { case (l, r) => l.size == r.size }
-              if (innerSizeEq && outerSizeEq) pass
-              else failHere because s"Number of parameter lists does not match"
-            }
-            prmsEq <- eqSeqns(vpss$l.flatten, vpss$r.flatten)
-            rhssEq <- eqTrees(rhs$l, rhs$r)
-          } yield pass
+      case (
+        CaseDef(pat$l, guard$l, body$l),
+        CaseDef(pat$r, guard$r, body$r)
+        ) => for {
+          eq <- trees(pat$l, pat$r)
+          eq <- trees(guard$l, guard$r)
+          eq <- trees(body$l, body$r)
+        } yield eq
 
-        case (
-          lhs@Match(selector$l, cases$l),
-          rhs@Match(selector$r, cases$r)
-          ) =>
-          for {
-            sltrEq <- eqTrees(selector$l, selector$r)
-            sizeEq <- {
-              if (cases$l.size == cases$r.size) pass
-              else failHere because s"Number of cases does not match"
-            }
-            argsEq <- eqSeqns(cases$l, cases$r)
-          } yield pass
+      case (
+        Bind(_, body$l),
+        Bind(_, body$r)
+        ) => for {
+          eq <- pass
+          _ = dict += lhs.symbol -> rhs.symbol
+          eq <- trees(body$l, body$r)
+        } yield eq
 
-        case (
-          lhs@CaseDef(pat$l, guard$l, body$l),
-          rhs@CaseDef(pat$r, guard$r, body$r)) =>
-          for {
-            patsEq <- eqTrees(pat$l, pat$r)
-            grdsEq <- eqTrees(guard$l, guard$r)
-            bodyEq <- eqTrees(body$l, body$r)
-          } yield pass
-
-        case (
-          lhs@Bind(_, body$l),
-          rhs@Bind(_, body$r)) =>
-          dict += lhs.symbol -> rhs.symbol
-          for {
-            exprEq <- eqTrees(body$l, body$r)
-          } yield pass
-
-        case _ =>
-          failHere because "Unexpected pair of trees"
-      }
+      case _ =>
+        failHere because "Unexpected pair of trees"
     }
 
-    def eqSymbs(lhs: Tree, rhs: Tree): Eq Or Neq = {
+    def symbols(lhs: Tree, rhs: Tree): Eq Or Neq = {
       import Term.name.{exprOwner, local}
 
-      def eqSymbs(lsm: Symbol, rsm: Symbol): Eq Or Neq = (lsm, rsm) match {
-        case (lsm: Symbol, rsm: Symbol) if lsm == rsm =>
+      def eqSymbols(lhSym: Symbol, rhSym: Symbol): Eq Or Neq = (lhSym, rhSym) match {
+        case (_, _) if lhSym == rhSym =>
           pass
 
-        case (lsm: FreeTermSymbol, rsm: FreeTermSymbol) =>
-          for {
-            nameEq <- {
-              if (lsm.name == lsm.name) pass
-              else failHere because "Free symbol names do not match"
-            }
-            vlueEq <- {
-              if (lsm.value == lsm.value) pass
-              else failHere because "Free symbol values do not match"
-            }
-          } yield pass
+        case (lhSym: FreeTermSymbol, rhSym: FreeTermSymbol) => for {
+          eq <- {
+            if (lhSym.name == lhSym.name) pass
+            else failHere because "Free symbol names do not match"
+          }
+          eq <- {
+            if (lhSym.value == lhSym.value) pass
+            else failHere because "Free symbol values do not match"
+          }
+        } yield eq
 
         case (Term.sym(`exprOwner`, _), Term.sym(`exprOwner`, _)) =>
           pass
@@ -324,45 +278,54 @@ trait AlphaEq extends Common {
         case (Term.sym(`local`, _), Term.sym(`local`, _)) =>
           pass
 
-        case _ =>
-          for {
-            contEq <- {
-              if (dict contains lsm) pass
-              else failHere because s"No alpha-mapping for symbol $lsm available"
-            }
-            symbEq <- {
-              if (dict(lsm) == rsm) pass
-              else failHere because s"Symbols $lsm and $lsm are not alpha equivalent"
-            }
-            ownrEq <- eqSymbs(lsm.owner, rsm.owner)
-          } yield pass
+        case _ => for {
+          eq <- {
+            if (dict contains lhSym) pass
+            else failHere because s"No alpha-mapping for symbol $lhSym available"
+          }
+          eq <- {
+            if (dict(lhSym) == rhSym) pass
+            else failHere because s"Symbols $lhSym and $lhSym are not alpha equivalent"
+          }
+          eq <- eqSymbols(lhSym.owner, rhSym.owner)
+        } yield eq
       }
 
-      eqSymbs(lhs.symbol, rhs.symbol)
+      eqSymbols(lhs.symbol, rhs.symbol)
     }
 
-    def eqNames(lnm: Name, rnm: Name)(lhs: Tree, rhs: Tree): Eq Or Neq =
-      if (lnm == rnm) pass
-      else failHere because s"Names $lhs and $rhs are not equal"
+    def names(lhName: Name, rhName: Name): Eq Or Neq =
+      if (lhName == rhName) pass
+      else failHere because s"Names $lhName and $rhName are not equal"
 
-    def eqTypes(lhs: Tree, rhs: Tree): Eq Or Neq =
+    def types(lhs: Tree, rhs: Tree): Eq Or Neq =
       if (lhs.tpe =:= rhs.tpe) pass
       else failHere because s"Types ${lhs.tpe} and ${rhs.tpe} are not equal"
 
-    def eqLtrls(lhs: Literal, rhs: Literal): Eq Or Neq =
-      if (lhs == rhs) pass
-      else failHere because "Literals are not equal"
+    def literals(lhs: Literal, rhs: Literal): Eq Or Neq =
+      if (lhs.value == rhs.value) pass
+      else failHere because s"Literals ${lhs.value} and ${rhs.value} are not equal"
 
-    def eqFlags(lfg: FlagSet, rfg: FlagSet)(lhs: Tree, rhs: Tree): Eq Or Neq =
-      if ((lfg | Flag.SYNTHETIC) == (rfg | Flag.SYNTHETIC)) pass
-      else failHere because s"Flag sets $lfg and $rfg are not equal"
+    def modifiers(lhMods: Modifiers, rhMods: Modifiers): Eq Or Neq =
+      if ((lhMods.flags | Flag.SYNTHETIC) == (rhMods.flags | Flag.SYNTHETIC)) pass
+      else failHere because s"$lhMods and $rhMods are not equal"
 
-    def eqSeqns(xs: Seq[Tree], ys: Seq[Tree]): Eq Or Neq =
-      (xs zip ys).foldLeft(pass)((res, pair) =>
-        if (res.isBad) res
-        else (eqTrees _).tupled(pair))
+    def all(lhTrees: Seq[Tree], rhTrees: Seq[Tree]): Eq Or Neq =
+      (lhTrees zip rhTrees).foldLeft(pass) { case (result, (x, y)) =>
+        if (result.isBad) result else trees(x, y)
+      }
 
-    eqTrees(lhs, rhs)
+    def size(lhTrees: Seq[Tree], rhTrees: Seq[Tree], children: String): Eq Or Neq =
+      if (lhTrees.size == rhTrees.size) pass
+      else failHere because s"Number of $children does not match"
+
+    def shape(lhTrees: Seq[Seq[Tree]], rhTrees: Seq[Seq[Tree]], children: String): Eq Or Neq = {
+      lazy val outerSizeEq = lhTrees.size == rhTrees.size
+      lazy val innerSizeEq = (lhTrees zip rhTrees) forall { case (l, r) => l.size == r.size }
+      if (outerSizeEq && innerSizeEq) pass
+      else failHere because s"Shape of $children does not match"
+    }
+
+    trees(lhs, rhs)
   }
-
 }
