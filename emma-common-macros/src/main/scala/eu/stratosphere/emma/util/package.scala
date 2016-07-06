59cae1d1f691ec68275684c7b88f9591e8801a34
Georgi Krastev <joro.kr.21@gmail.com>
2016-07-06 14:06:20 +0200

    [UTIL] Added alias types to emma.util

--- emma-common-macros/src/main/scala/eu/stratosphere/emma/util/package.scala
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/util/package.scala
@@ -1,7 +1,12 @@
 package eu.stratosphere
 package emma
 
+import shapeless.labelled.FieldType
+
 package object util {
+
+  type =?>[-A, +R] = PartialFunction[A, R]
+  type ->>[K, +V] = FieldType[K, V]
 
   /** Boolean logic with predicates. */
   implicit class Predicate[A](val self: A => Boolean) extends AnyVal {
