1fb231841dffa439aec9ba097e1ce92283cb7e36
Georgi Krastev <joro.kr.21@gmail.com>
2016-07-27 19:40:21 +0200

    [TEST] Made TreeMatchers generic in the tree type
    Easier to deal with for IDEs.

--- emma-language/src/test/scala/eu/stratosphere/emma/compiler/lang/TreeMatchers.scala
+++ emma-language/src/test/scala/eu/stratosphere/emma/compiler/lang/TreeMatchers.scala
@@ -5,6 +5,7 @@ package lang
 import org.scalactic._
 import org.scalatest.matchers._
 
+/** [[org.scalatest]] matchers for Scala ASTs. */
 trait TreeMatchers {
 
   val compiler: Compiler
@@ -12,27 +13,30 @@ trait TreeMatchers {
   import compiler._
   import universe._
 
-  def alphaEqTo(rhs: Tree) = new AlphaEqMatcher(rhs)
+  /** Test that the LHS tree is alpha-equivalent to the RHS tree. */
+  def alphaEqTo[T <: Tree](rhs: T) =
+    new AlphaEqMatcher(rhs)
 
-  class AlphaEqMatcher(rhs: Tree) extends BeMatcher[Tree] {
-    def apply(lhs: Tree) = {
-      // message constructor function
-      def msg(err: Neq, negated: Boolean) =
-        s"""
-           |Left tree ${if (negated) "was" else "was not"} alpha equivalent to right tree.
-           |
-           |${asSource("lhs")(lhs)}
-           |${asSource("rhs")(rhs)}
-           |
-           |due to the following difference
-           |
-           |  "${err.msg}"
-           |
-           |detected at the following subtrees
-           |
-           |${asSource("lhs subtree")(err.lhs)}
-           |${asSource("rhs subtree")(err.rhs)}
-        """.stripMargin
+  /** [[u.Tree]] matcher for alpha equivalence. */
+  class AlphaEqMatcher(rhs: u.Tree) extends BeMatcher[u.Tree] {
+
+    override def apply(lhs: u.Tree): MatchResult = {
+      // Message helper function
+      def msg(err: Neq, negated: Boolean) = s"""
+         |Left tree ${if (negated) "was" else "was not"} alpha equivalent to right tree.
+         |
+         |${asSource("lhs")(lhs)}
+         |${asSource("rhs")(rhs)}
+         |
+         |due to the following difference
+         |
+         |  "${err.msg}"
+         |
+         |detected at the following subtrees
+         |
+         |${asSource("lhs subtree")(err.lhs)}
+         |${asSource("rhs subtree")(err.rhs)}
+      """.stripMargin
 
       compiler.alphaEq(lhs, rhs) match {
         case Good(_) =>
@@ -43,12 +47,13 @@ trait TreeMatchers {
     }
   }
 
+  /** Matcher for accumulating [[org.scalactic.Or]] that prints all errors. */
   object good extends BeMatcher[Any Or Every[Any]] {
     override def apply(or: Any Or Every[Any]): MatchResult = or match {
       case Good(ok) =>
         MatchResult(matches = true, "", s"$ok was good")
       case Bad(errors) =>
-        MatchResult(matches = false, errors mkString "\n\n", "")
+        MatchResult(matches = false, errors.mkString("\n\n"), "")
     }
   }
 }
