2ee6ea3b48dca507ac746a6508ff2d7997880430
Georgi Krastev <joro.kr.21@gmail.com>
2016-08-19 15:05:56 +0200

    [TEST] Added `BaseCompilerSpec.normalizeLet` transformation.

--- emma-language/src/test/scala/eu/stratosphere/emma/compiler/BaseCompilerSpec.scala
+++ emma-language/src/test/scala/eu/stratosphere/emma/compiler/BaseCompilerSpec.scala
@@ -4,6 +4,8 @@ package compiler
 import api.DataBag
 import lang.TreeMatchers
 
+import shapeless._
+
 import org.scalatest.prop.PropertyChecks
 import org.scalatest.{FreeSpec, Matchers}
 
@@ -16,6 +18,7 @@ trait BaseCompilerSpec extends FreeSpec with Matchers with PropertyChecks with T
 
   import compiler._
   import universe._
+  import Core.{Lang => core}
 
   // ---------------------------------------------------------------------------
   // Common transformation pipelines
@@ -26,6 +29,15 @@ trait BaseCompilerSpec extends FreeSpec with Matchers with PropertyChecks with T
   } andThen {
     Type.check(_: Tree)
   }
+
+  /** Apply after [[Core.anf()]] for testing purposes. */
+  lazy val normalizeLet: u.Tree => u.Tree =
+    api.BottomUp.withParent.transformWith {
+      case Attr.inh(core.Let(Seq(), Seq(), expr), core.Branch(_, _, _) :: _) => expr
+      case Attr.inh(let @ core.Let(_, _, _), core.DefDef(_, _, _, _, _) :: _) => let
+      case Attr.inh(core.Atomic(expr), core.DefDef(_, _, _, _, _) :: _) =>
+        core.Let()()(expr).asInstanceOf[u.Tree]
+    }.andThen(_.tree)
 
   // ---------------------------------------------------------------------------
   // Common value definitions used in compiler tests
