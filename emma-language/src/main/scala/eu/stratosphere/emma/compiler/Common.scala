894b969b30512c3a4f79bf4896aeae3a494c25a5
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-08-22 22:52:20 +0200

    [IR] Re-formatted `Common.API` and `Common.IR`.

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/Common.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/Common.scala
@@ -17,29 +17,30 @@ trait Common extends ReflectUtil with AST {
 
   /** A set of API method symbols to be comprehended. */
   protected[emma] object API {
-    val moduleSymbol /*    */ = rootMirror.staticModule("eu.stratosphere.emma.api.package")
-    val bagSymbol /*       */ = rootMirror.staticClass("eu.stratosphere.emma.api.DataBag")
-    val groupSymbol /*     */ = rootMirror.staticClass("eu.stratosphere.emma.api.Group")
-    val statefulSymbol /*  */ = rootMirror.staticClass("eu.stratosphere.emma.api.Stateful.Bag")
-    val inputFmtSymbol /*  */ = rootMirror.staticClass("eu.stratosphere.emma.api.InputFormat")
-    val outputFmtSymbol /* */ = rootMirror.staticClass("eu.stratosphere.emma.api.OutputFormat")
+    //@formatter:off
+    val moduleSymbol          = rootMirror.staticModule("eu.stratosphere.emma.api.package")
+    val bagSymbol             = rootMirror.staticClass("eu.stratosphere.emma.api.DataBag")
+    val groupSymbol           = rootMirror.staticClass("eu.stratosphere.emma.api.Group")
+    val statefulSymbol        = rootMirror.staticClass("eu.stratosphere.emma.api.Stateful.Bag")
+    val inputFmtSymbol        = rootMirror.staticClass("eu.stratosphere.emma.api.InputFormat")
+    val outputFmtSymbol       = rootMirror.staticClass("eu.stratosphere.emma.api.OutputFormat")
 
-    val apply /*           */ = bagSymbol.companion.info.decl(TermName("apply"))
-    val read /*            */ = moduleSymbol.info.decl(TermName("read"))
-    val write /*           */ = moduleSymbol.info.decl(TermName("write"))
-    val stateful /*        */ = moduleSymbol.info.decl(TermName("stateful"))
-    val fold /*            */ = bagSymbol.info.decl(TermName("fold"))
-    val map /*             */ = bagSymbol.info.decl(TermName("map"))
-    val flatMap /*         */ = bagSymbol.info.decl(TermName("flatMap"))
-    val withFilter /*      */ = bagSymbol.info.decl(TermName("withFilter"))
-    val groupBy /*         */ = bagSymbol.info.decl(TermName("groupBy"))
-    val minus /*           */ = bagSymbol.info.decl(TermName("minus"))
-    val plus /*            */ = bagSymbol.info.decl(TermName("plus"))
-    val distinct /*        */ = bagSymbol.info.decl(TermName("distinct"))
-    val fetchToStateless /**/ = statefulSymbol.info.decl(TermName("bag"))
-    val updateWithZero /*  */ = statefulSymbol.info.decl(TermName("updateWithZero"))
-    val updateWithOne /*   */ = statefulSymbol.info.decl(TermName("updateWithOne"))
-    val updateWithMany /*  */ = statefulSymbol.info.decl(TermName("updateWithMany"))
+    val apply                 = bagSymbol.companion.info.decl(TermName("apply"))
+    val read                  = moduleSymbol.info.decl(TermName("read"))
+    val write                 = moduleSymbol.info.decl(TermName("write"))
+    val stateful              = moduleSymbol.info.decl(TermName("stateful"))
+    val fold                  = bagSymbol.info.decl(TermName("fold"))
+    val map                   = bagSymbol.info.decl(TermName("map"))
+    val flatMap               = bagSymbol.info.decl(TermName("flatMap"))
+    val withFilter            = bagSymbol.info.decl(TermName("withFilter"))
+    val groupBy               = bagSymbol.info.decl(TermName("groupBy"))
+    val minus                 = bagSymbol.info.decl(TermName("minus"))
+    val plus                  = bagSymbol.info.decl(TermName("plus"))
+    val distinct              = bagSymbol.info.decl(TermName("distinct"))
+    val fetchToStateless      = statefulSymbol.info.decl(TermName("bag"))
+    val updateWithZero        = statefulSymbol.info.decl(TermName("updateWithZero"))
+    val updateWithOne         = statefulSymbol.info.decl(TermName("updateWithOne"))
+    val updateWithMany        = statefulSymbol.info.decl(TermName("updateWithMany"))
 
     val methods = Set(
       read, write,
@@ -54,20 +55,23 @@ trait Common extends ReflectUtil with AST {
     val updateWith = Set(updateWithZero, updateWithOne, updateWithMany)
 
     // Type constructors
-    val DATA_BAG /*        */ = typeOf[eu.stratosphere.emma.api.DataBag[Nothing]].typeConstructor
-    val GROUP /*           */ = typeOf[eu.stratosphere.emma.api.Group[Nothing, Nothing]].typeConstructor
+    val DATA_BAG              = typeOf[eu.stratosphere.emma.api.DataBag[Nothing]].typeConstructor
+    val GROUP                 = typeOf[eu.stratosphere.emma.api.Group[Nothing, Nothing]].typeConstructor
+    //@formatter:on
   }
 
   protected[emma] object IR {
-    val module /*          */ = rootMirror.staticModule("eu.stratosphere.emma.compiler.ir.package").asModule
+    //@formatter:off
+    val module                = rootMirror.staticModule("eu.stratosphere.emma.compiler.ir.ComprehensionSyntax").asModule
 
-    val flatten /*         */ = module.info.decl(TermName("flatten")).asMethod
-    val generator /*       */ = module.info.decl(TermName("generator")).asMethod
-    val comprehension /*   */ = module.info.decl(TermName("comprehension")).asMethod
-    val guard /*           */ = module.info.decl(TermName("guard")).asMethod
-    val head /*            */ = module.info.decl(TermName("head")).asMethod
+    val flatten               = module.info.decl(TermName("flatten")).asMethod
+    val generator             = module.info.decl(TermName("generator")).asMethod
+    val comprehension         = module.info.decl(TermName("comprehension")).asMethod
+    val guard                 = module.info.decl(TermName("guard")).asMethod
+    val head                  = module.info.decl(TermName("head")).asMethod
 
-    val comprehensionOps /**/ = Set(flatten, generator, comprehension, guard, head)
+    val comprehensionOps      = Set(flatten, generator, comprehension, guard, head)
+    //@formatter:on
   }
 
   /** Common validation helpers. */
