97fc8e9516a3340e98904b9b5eaed4da02da5751
Georgi Krastev <joro.kr.21@gmail.com>
2016-08-26 08:50:44 +0200

    [UTIL] Added util.Functions and removed util.Predicates

---
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/util/Functions.scala
@@ -0,0 +1,16 @@
+package eu.stratosphere.emma
+package util
+
+import shapeless._
+
+/** Utilities for [[scala.Function]]s. */
+object Functions {
+
+  /** Applies a partial function to the `args` or else return `default`. */
+  def complete[A, R](pf: A =?> R)(args: A)(default: => R): R =
+    pf.applyOrElse(args, (_: A) => default)
+
+  /** Forgets the head of the returned [[HList]]. */
+  def tail[A, H, T <: HList](f: A => (H :: T)): A => T =
+    f.andThen(_.tail)
+}
--- emma-common-macros/src/main/scala/eu/stratosphere/emma/util/package.scala
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/util/package.scala
@@ -7,20 +7,4 @@ package object util {
 
   type =?>[-A, +R] = PartialFunction[A, R]
   type ->>[K, +V] = FieldType[K, V]
-
-  /** Boolean logic with predicates. */
-  implicit class Predicate[A](val self: A => Boolean) extends AnyVal {
-
-    /** Negates this predicate. */
-    def unary_! : A => Boolean =
-      self.andThen(!_)
-
-    /** Combines this predicate with `that` via conjunction. */
-    def &&[B >: A](that: B => Boolean): A => Boolean =
-      x => self(x) && that(x)
-
-    /** Combines this predicate with `that` via disjunction. */
-    def ||[B >: A](that: B => Boolean): A => Boolean =
-      x => self(x) || that(x)
-  }
 }
