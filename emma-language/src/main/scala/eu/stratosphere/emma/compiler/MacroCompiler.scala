89c17da31c6553688976ecbdf76863267d90b865
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2016-09-02 13:13:43 +0300

    [IR] Refactored `MacroCompiler` as trait.
    The `MacroCompiler` should be mixed in as a trait in the top-level blackbox
    macro, in order to avoid path-dependent type ambiguities in the `c` context
    resulting from explicit compiler instantiation.

--- emma-language/src/main/scala/eu/stratosphere/emma/compiler/MacroCompiler.scala
+++ emma-language/src/main/scala/eu/stratosphere/emma/compiler/MacroCompiler.scala
@@ -6,4 +6,4 @@ import ast.MacroAST
 import scala.reflect.macros.blackbox
 
 /** A macro-based [[Compiler]]. */
-class MacroCompiler(val c: blackbox.Context) extends Compiler with MacroAST
+trait MacroCompiler extends Compiler with MacroAST
