606c8df14997ce7d36634c15d7e28e1e10f782e5
Georgi Krastev <joro.kr.21@gmail.com>
2016-09-08 07:48:39 +0300

    [UTIL] Added type-level IsEmpty op for HLists

---
+++ emma-common-macros/src/main/scala/eu/stratosphere/emma/util/IsEmpty.scala
@@ -0,0 +1,22 @@
+package eu.stratosphere
+package emma.util
+
+import shapeless._
+
+/** Type-level `isEmpty` for [[shapeless.HList]]. */
+trait IsEmpty[L <: HList] {
+  def apply(): Boolean
+}
+
+object IsEmpty {
+
+  /** [[shapeless.HNil]] is empty. */
+  implicit val hNil: IsEmpty[HNil] = new IsEmpty[HNil] {
+    override def apply(): Boolean = true
+  }
+
+  /** [[shapeless.::]] (H-Cons) is non-empty. */
+  implicit def hCons[H, T <: HList]: IsEmpty[H :: T] = new IsEmpty[H :: T] {
+    override def apply(): Boolean = false
+  }
+}
