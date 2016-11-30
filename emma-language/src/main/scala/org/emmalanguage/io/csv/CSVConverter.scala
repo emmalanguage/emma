7cc01a3a4bf462dd2598c76df087a18910a54067
Georgi Krastev <joro.kr.21@gmail.com>
2016-11-30 19:49:58 +0100

    [IO] Optimized CSVConverter to share record arrays
    No copying of arrays is necessary for reading / writing CSV.

--- emma-language/src/main/scala/org/emmalanguage/io/csv/CSVConverter.scala
+++ emma-language/src/main/scala/org/emmalanguage/io/csv/CSVConverter.scala
@@ -20,7 +20,7 @@ import api._
 
 import shapeless._
 
-import scala.Function.const
+import scala.collection.breakOut
 import scala.collection.generic.CanBuild
 import scala.language.higherKinds
 import scala.reflect.ClassTag
@@ -29,7 +29,7 @@ import scala.reflect.ClassTag
  * Codec (provides encoding and decoding) type-class for CSV records.
  * A CSV record consists of a fixed number of columns, encoded as an array of Strings.
  *
- * This codec is unsafe. Upon failure (mostly when decoding), an exception may be thrown.
+ * This codec is unsafe. Upon failure (mostly when reading), an exception may be thrown.
  * A very common exception type is [[NumberFormatException]].
  *
  * Provided by default are codecs for Scala and Java primitives, enumerations, [[Option]],
@@ -41,14 +41,14 @@ trait CSVConverter[T] {
   def size: Int
 
   /**
-   * Decodes an instance of `T` from a CSV `record`.
-   * Decoding is unsafe as it may throw an exception if parsing failed.
+   * Reads an instance of `T` from a CSV `record` at position `i`.
+   * Decoding is unsafe as it may throw an exception if reading failed.
    * A very common exception type is [[NumberFormatException]].
    */
-  def from(record: Array[String])(implicit csv: CSV): T
+  def read(record: Array[String], i: Int)(implicit csv: CSV): T
 
-  /** Encodes an instance of `T` as a CSV record. */
-  def to(value: T)(implicit csv: CSV): Array[String]
+  /** Writes an instance of `T` in a CSV `record` at position `i`. */
+  def write(value: T, record: Array[String], i: Int)(implicit csv: CSV): Unit
 }
 
 /** Factory methods and implicit instances of CSV converters. */
@@ -58,51 +58,49 @@ object CSVConverter {
   def apply[T](implicit converter: CSVConverter[T]): CSVConverter[T] =
     converter
 
-  /** Creates a new codec for type `T` from pure conversion functions. */
-  def make[T](length: Int)(fromF: Array[String] => T, toF: T => Array[String])
-    : CSVConverter[T] = new CSVConverter[T] {
-      def size = length
-      def from(record: Array[String])(implicit csv: CSV) = fromF(record)
-      def to(value: T)(implicit csv: CSV) = toF(value)
-    }
-
   /**
    * Derives a new codec for type `B` from an existing codec for type `A`,
    * given an isomorphism between `A` and `B`.
    */
-  def iso[A, B](fromF: A => B, toF: B => A)(implicit A: CSVConverter[A]): CSVConverter[B] =
+  def iso[A, B](from: A => B, to: B => A)(implicit A: CSVConverter[A]): CSVConverter[B] =
     new CSVConverter[B] {
       def size = A.size
-      def from(record: Array[String])(implicit csv: CSV) = fromF(A from record)
-      def to(value: B)(implicit csv: CSV) = A to toF(value)
+      def read(record: Array[String], i: Int)(implicit csv: CSV) =
+        from(A.read(record, i))
+      def write(value: B, record: Array[String], i: Int)(implicit csv: CSV) =
+        A.write(to(value), record, i)
     }
 
   /** Creates a codec with a single column. */
   implicit def singleColumnCSVConverter[T](implicit T: CSVColumn[T]): CSVConverter[T] =
     new CSVConverter[T] {
       def size = 1
-      def from(record: Array[String])(implicit csv: CSV) = T from record.head
-      def to(value: T)(implicit csv: CSV) = Array(T to value)
+      def read(record: Array[String], i: Int)(implicit csv: CSV) =
+        T.from(record(i))
+      def write(value: T, record: Array[String], i: Int)(implicit csv: CSV) =
+        record(i) = T.to(value)
     }
 
   /** An empty codec for [[HNil]]. */
   implicit val hNilCSVConverter: CSVConverter[HNil] =
-    make(0)(const(HNil), const(Array.empty))
+    new CSVConverter[HNil] {
+      def size = 0
+      def read(record: Array[String], i: Int)(implicit csv: CSV) = HNil
+      def write(value: HNil, record: Array[String], i: Int)(implicit csv: CSV) = ()
+    }
 
   /** Inductively generates a codec for an [[HList]], given codecs for its elements. */
   implicit def hConsCSVConverter[H, T <: HList](
     implicit H: Lazy[CSVConverter[H]], T: Lazy[CSVConverter[T]]
   ): CSVConverter[H :: T] = new CSVConverter[H :: T] {
-    val size = H.value.size + T.value.size
-
-    def from(record: Array[String])(implicit csv: CSV) = {
-      val (h, t) = record splitAt H.value.size
-      H.value.from(h) :: T.value.from(t)
-    }
-
-    def to(value: H :: T)(implicit csv: CSV) = {
-      val h :: t = value
-      H.value.to(h) ++ T.value.to(t)
+    private def h = H.value
+    private def t = T.value
+    val size = h.size + t.size
+    def read(record: Array[String], i: Int)(implicit csv: CSV) =
+      h.read(record, i) :: t.read(record, i + h.size)
+    def write(value: H :: T, record: Array[String], i: Int)(implicit csv: CSV) = {
+      h.write(value.head, record, i)
+      t.write(value.tail, record, i + h.size)
     }
   }
 
@@ -110,9 +108,12 @@ object CSVConverter {
   implicit def genericCSVConverter[T, R <: HList](
     implicit gen: Generic.Aux[T, R], R: Lazy[CSVConverter[R]]
   ): CSVConverter[T] = new CSVConverter[T] {
-    def size = R.value.size
-    def from(record: Array[String])(implicit csv: CSV) = gen.from(R.value from record)
-    def to(value: T)(implicit csv: CSV) = R.value.to(gen to value)
+    private def r = R.value
+    def size = r.size
+    def read(record: Array[String], i: Int)(implicit csv: CSV) =
+      gen.from(r.read(record, i))
+    def write(value: T, record: Array[String], i: Int)(implicit csv: CSV) =
+      r.write(gen.to(value), record, i)
   }
 
   /**
@@ -122,10 +123,13 @@ object CSVConverter {
    */
   implicit def arrayCSVConverter[A](
     implicit A: CSVColumn[A], ctag: ClassTag[A]
-  ): CSVConverter[Array[A]] = singleColumnCSVConverter(new CSVColumn[Array[A]] {
-    def from(column: String)(implicit csv: CSV) = column split csv.delimitSeq map A.from
-    def to(value: Array[A])(implicit csv: CSV) = value map A.to mkString csv.delimitSeq.toString
-  })
+  ): CSVConverter[Array[A]] = singleColumnCSVConverter(
+    new CSVColumn[Array[A]] {
+      def from(column: String)(implicit csv: CSV) =
+        column.split(csv.delimitSeq).map(A.from)
+      def to(value: Array[A])(implicit csv: CSV) =
+        value.iterator.map(A.to).mkString(csv.delimitSeq.toString)
+    })
 
   /**
    * Creates a codec for a [[Traversable]] collection from an element column.
@@ -133,10 +137,13 @@ object CSVConverter {
    */
   implicit def traversableCSVConverter[T[x] <: Traversable[x], A](
     implicit TA: CanBuild[A, T[A]], A: CSVColumn[A]
-  ): CSVConverter[T[A]] = singleColumnCSVConverter(new CSVColumn[T[A]] {
-    def from(column: String)(implicit csv: CSV) = column split csv.delimitSeq map A.from to TA
-    def to(value: T[A])(implicit csv: CSV) = value map A.to mkString csv.delimitSeq.toString
-  })
+  ): CSVConverter[T[A]] = singleColumnCSVConverter(
+    new CSVColumn[T[A]] {
+      def from(column: String)(implicit csv: CSV) =
+        column.split(csv.delimitSeq).map(A.from)(breakOut)
+      def to(value: T[A])(implicit csv: CSV) =
+        value.toIterator.map(A.to).mkString(csv.delimitSeq.toString)
+    })
 
   /**
    * Creates a [[DataBag]] codec from an element column.
