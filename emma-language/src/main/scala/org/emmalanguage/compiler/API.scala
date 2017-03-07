04ffbc99c2783463949a86e6da16cbf7320a6141
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2017-03-07 17:12:30 +0100

    [IR] Reworked the Emma API reflected in the compiler.
    Re-organized the mess currently residing in `Common`.
    
    API methods are now accessable through a standard interface.
    
    ```scala
    API.DataBag.map
    API.DataBag$.readCSV
    API.GraphRepresentation.phi
    ```
    
    The next commits will deprecate the old API and introduce the
    use of `_API_.*` terms instead, and then batch-rename `_API_`
    to `API` in a single commit.
    
    Backend compilers (such as `FlinkCompiler` and `SparkCompiler`)
    will define their own refined APIs in a similar manner once the
    main refactoring is completed.

---
+++ emma-language/src/main/scala/org/emmalanguage/compiler/API.scala
@@ -0,0 +1,252 @@
+/*
+ * Copyright © 2014 TU Berlin (emma@dima.tu-berlin.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.emmalanguage
+package compiler
+
+import ast.AST
+
+/** Common IR tools. */
+protected[emmalanguage] trait API extends AST {
+
+  import org.emmalanguage.{api => eapi}
+
+  trait ReflectedSymbol[Symbol <: u.Symbol] {
+    def sym: Symbol
+
+    def ops: Set[u.MethodSymbol]
+
+    protected def op(name: String): u.MethodSymbol =
+      sym.info.member(api.TermName(name)).asMethod
+
+    protected def ann(sym: u.ClassSymbol) =
+      u.Annotation(api.Inst(sym.toType, argss = Seq(Seq.empty)))
+  }
+
+  trait ClassAPI extends ReflectedSymbol[u.ClassSymbol] {
+    def tpe = api.Type.constructor(sym.info)
+  }
+
+  trait ModuleAPI extends ReflectedSymbol[u.ModuleSymbol]
+
+  trait emma$API extends ModuleAPI {
+    //@formatter:off
+    val sym                   = api.Sym[eapi.emma.`package`.type].asModule
+
+    val prettyPrint           = op("prettyPrint")
+    val quote                 = op("quote")
+
+    val ops                   = Set(prettyPrint, quote)
+    //@formatter:on
+  }
+
+  class DataBagAPI(cls: u.ClassSymbol) extends ClassAPI {
+
+    //@formatter:off
+    val sym                   = cls
+
+    // Sinks
+    val fetch                 = op("fetch")
+    val as                    = op("as")
+    val writeCSV              = op("writeCSV")
+    val writeParquet          = op("writeParquet")
+    val writeText             = op("writeText")
+    // Monad ops
+    val map                   = op("map")
+    val flatMap               = op("flatMap")
+    val withFilter            = op("withFilter")
+    // Grouping
+    val groupBy               = op("groupBy")
+    // Set operations
+    val union                 = op("union")
+    val distinct              = op("distinct")
+    // Structural recursion & Folds
+    val fold                  = op("fold")
+    val isEmpty               = op("isEmpty")
+    val nonEmpty              = op("nonEmpty")
+    val reduce                = op("reduce")
+    val reduceOption          = op("reduceOption")
+    val min                   = op("min")
+    val max                   = op("max")
+    val sum                   = op("sum")
+    val product               = op("product")
+    val size                  = op("size")
+    val count                 = op("count")
+    val exists                = op("exists")
+    val forall                = op("forall")
+    val find                  = op("find")
+    val bottom                = op("bottom")
+    val top                   = op("top")
+    val sample                = op("sample")
+
+    val sinkOps               = Set(fetch, as, writeCSV, writeParquet, writeText)
+    val monadOps              = Set(map, flatMap, withFilter)
+    val nestOps               = Set(groupBy)
+    val boolAlgOps            = Set(union, distinct)
+    val foldOps               = Set(
+      fold,
+      isEmpty, nonEmpty,
+      reduce, reduceOption,
+      min, max, sum, product,
+      size, count,
+      exists, forall,
+      find, top, bottom,
+      sample
+    )
+
+    val ops = sinkOps | monadOps | nestOps | boolAlgOps | foldOps
+    //@formatter:on
+  }
+
+  class DataBag$API(mod: u.ModuleSymbol) extends ModuleAPI {
+    //@formatter:off
+    val sym                   = mod
+
+    val from                  = op("from")
+    val empty                 = op("empty")
+    val apply                 = op("apply")
+    val readCSV               = op("readCSV")
+    val readParquet           = op("readParquet")
+    val readText              = op("readText")
+
+    val ops                   = Set(from, empty, apply, readCSV, readParquet, readText)
+    //@formatter:on
+  }
+
+  class MutableBagAPI(cls: u.ClassSymbol) extends ClassAPI {
+    //@formatter:off
+    val sym                   = cls
+
+    val update                = op("update")
+    val bag                   = op("bag")
+    val copy                  = op("copy")
+
+    val ops                   = Set(update, bag, copy)
+    //@formatter:on
+  }
+
+  class MutableBag$API(mod: u.ModuleSymbol) extends ModuleAPI {
+    //@formatter:off
+    val sym                   = mod
+
+    val apply                 = op("apply")
+
+    val ops                   = Set(apply)
+    //@formatter:on
+  }
+
+  trait ComprehensionCombinatorsAPI extends ModuleAPI {
+    //@formatter:off
+    val cross                 = op("cross")
+    val equiJoin              = op("equiJoin")
+
+    def ops                   = Set(cross, equiJoin)
+    //@formatter:on
+  }
+
+  trait RuntimeAPI extends ModuleAPI {
+    //@formatter:off
+    val cache                 = op("cache")
+
+    def ops                   = Set(cache)
+    //@formatter:on
+  }
+
+  class BackendOpsAPI(mod: u.ModuleSymbol) extends ComprehensionCombinatorsAPI with RuntimeAPI {
+    //@formatter:off
+    val sym                   = mod
+
+    override val ops          = super[ComprehensionCombinatorsAPI].ops ++ super[RuntimeAPI].ops
+    //@formatter:on
+  }
+
+  trait ComprehensionSyntaxAPI extends ModuleAPI {
+    //@formatter:off
+    val sym                 = api.Sym[ir.ComprehensionSyntax.type].asModule
+
+    val flatten             = op("flatten")
+    val generator           = op("generator")
+    val comprehension       = op("comprehension")
+    val guard               = op("guard")
+    val head                = op("head")
+
+    val ops                 = Set(flatten, generator, comprehension, guard, head)
+    //@formatter:on
+  }
+
+  trait GraphRepresentationAPI extends ModuleAPI {
+    //@formatter:off
+    val sym                   = api.Sym[ir.GraphRepresentation.type].asModule
+
+    val phi                   = op("phi")
+
+    val ops                   = Set(phi)
+    //@formatter:on
+  }
+
+  trait DSCFAnnotationsAPI extends ModuleAPI {
+    //@formatter:off
+    val sym                   = api.Sym[ir.DSCFAnnotations.type].asModule
+
+    // Annotation symbols
+    val branch                = api.Sym[ir.DSCFAnnotations.branch].asClass
+    val loop                  = api.Sym[ir.DSCFAnnotations.loop].asClass
+    val suffix                = api.Sym[ir.DSCFAnnotations.suffix].asClass
+    val thenBranch            = api.Sym[ir.DSCFAnnotations.thenBranch].asClass
+    val elseBranch            = api.Sym[ir.DSCFAnnotations.elseBranch].asClass
+    val whileLoop             = api.Sym[ir.DSCFAnnotations.whileLoop].asClass
+    val doWhileLoop           = api.Sym[ir.DSCFAnnotations.doWhileLoop].asClass
+    val loopBody              = api.Sym[ir.DSCFAnnotations.loopBody].asClass
+
+    // Annotation trees
+    val suffixAnn             = ann(suffix)
+    val thenAnn               = ann(thenBranch)
+    val elseAnn               = ann(elseBranch)
+    val whileAnn              = ann(whileLoop)
+    val doWhileAnn            = ann(doWhileLoop)
+    val loopBodyAnn           = ann(loopBody)
+
+    val ops                   = Set.empty[u.MethodSymbol]
+    //@formatter:on
+  }
+
+  /** Reflection of the Emma API. */
+  object _API_ {
+
+    object emma$ extends emma$API
+
+    object DataBag extends DataBagAPI(api.Sym[eapi.DataBag[Any]].asClass)
+
+    object DataBag$ extends DataBag$API(api.Sym[eapi.DataBag.type].asModule)
+
+    object ScalaSeq extends DataBagAPI(api.Sym[eapi.ScalaSeq[Any]].asClass)
+
+    object ScalaSeq$ extends DataBag$API(api.Sym[eapi.ScalaSeq.type].asModule)
+
+    object MutableBag extends MutableBagAPI(api.Sym[eapi.MutableBag[Any, Any]].asClass)
+
+    object MutableBag$ extends MutableBag$API(api.Sym[eapi.MutableBag.type].asModule)
+
+    object Backend extends BackendOpsAPI(api.Sym[eapi.backend.Backend.type].asModule)
+
+    object ComprehensionSyntax extends ComprehensionSyntaxAPI
+
+    object GraphRepresentation extends GraphRepresentationAPI
+
+    object DSCFAnnotations extends DSCFAnnotationsAPI
+
+  }
+
+}
