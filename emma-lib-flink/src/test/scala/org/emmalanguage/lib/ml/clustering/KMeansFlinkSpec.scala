c8cd502130483d7840ea5b16f8ff040119cf3467
Alexander Alexandrov <alexander.s.alexandrov@gmail.com>
2017-08-09 15:00:33 +0200

    [LIB][ML] Changed `kMeans` implementation.
    Removed `epsilon` based termination criterion and
    introduced `runs` and `iterations` for a fixed number of
    outer and inner loops for more predicatble performance.

--- emma-lib-flink/src/test/scala/org/emmalanguage/lib/ml/clustering/KMeansFlinkSpec.scala
+++ emma-lib-flink/src/test/scala/org/emmalanguage/lib/ml/clustering/KMeansFlinkSpec.scala
@@ -23,7 +23,11 @@ import lib.ml._
 
 class KMeansFlinkSpec extends KMeansSpec with FlinkAware {
 
-  override def run(k: Int, epsilon: Double, iterations: Int, input: String): Set[kMeans.Solution[Long]] =
+  override val runs = 2
+  override val iterations = 2
+  override val overlap = .30
+
+  override def run(k: Int, input: String): Set[kMeans.Solution[Long]] =
     withDefaultFlinkEnv(implicit flink => emma.onFlink {
       // read the input
       val points = for (line <- DataBag.readText(input)) yield {
@@ -31,7 +35,7 @@ class KMeansFlinkSpec extends KMeansSpec with FlinkAware {
         DPoint(record.head.toLong, dense(record.tail.map(_.toDouble)))
       }
       // do the clustering
-      val result = kMeans(2, k, epsilon, iterations)(points)
+      val result = kMeans(2, k, runs, iterations)(points)
       // return the solution as a local set
       result.collect().toSet[kMeans.Solution[Long]]
     })
