3a7e91b206df3c2e2d905e389350e8e537171d29
Lorand Madai-Tahy <madai-tahy@campus.tu-berlin.de>
2018-07-13 16:19:16 +0200

    [LABY] Module structure and Scala versions of some Laby jobs

---
+++ emma-labyrinth/src/main/scala/org/emmalanguage/compiler/LabyrinthCompiler.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright © 2014 TU Berlin (emma@dima.tu-berlin.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.emmalanguage
+package compiler
+
+import com.typesafe.config.Config
+
+trait LabyrinthCompiler extends Compiler {
+
+  def transformations(implicit cfg: Config): Seq[TreeTransform] = Seq(
+    // lifting
+    Lib.expand,
+    Core.lift,
+    // optimizations
+    Core.cse iff "emma.compiler.opt.cse" is true,
+    Optimizations.foldFusion iff "emma.compiler.opt.fold-fusion" is true,
+    Optimizations.addCacheCalls iff "emma.compiler.opt.auto-cache" is true,
+    // backend
+    Comprehension.combine,
+    Core.unnest
+    // TODO
+
+//        SparkBackend.transform,
+//        SparkOptimizations.specializeOps iff "emma.compiler.spark.native-ops" is true,
+//
+//    // lowering
+//    Core.trampoline iff "emma.compiler.lower" is "trampoline",
+//
+//    // Core.dscfInv iff "emma.compiler.lower" is "dscfInv",
+//
+//    removeShadowedThis
+  ) filterNot (_ == noop)
+}
