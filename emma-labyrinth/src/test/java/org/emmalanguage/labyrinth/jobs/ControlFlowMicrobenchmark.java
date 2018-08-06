fe0d96cbb077a0ff8773bcbf5fce58206ba63400
Gabor Gevay <ggab90@gmail.com>
2018-08-06 17:26:47 +0200

    [LABY] Add ControlFlowMicrobenchmark

--- emma-labyrinth/src/test/java/org/emmalanguage/labyrinth/jobs/ControlFlowMicrobenchmark.java
+++ emma-labyrinth/src/test/java/org/emmalanguage/labyrinth/jobs/ControlFlowMicrobenchmark.java
@@ -16,19 +16,6 @@
 
 package org.emmalanguage.labyrinth.jobs;
 
-import org.emmalanguage.labyrinth.operators.IncMap;
-import org.emmalanguage.labyrinth.CFLConfig;
-import org.emmalanguage.labyrinth.ElementOrEvent;
-import org.emmalanguage.labyrinth.KickoffSource;
-import org.emmalanguage.labyrinth.LabyNode;
-import org.emmalanguage.labyrinth.LabySource;
-import org.emmalanguage.labyrinth.operators.AssertEquals;
-import org.emmalanguage.labyrinth.operators.ConditionNode;
-import org.emmalanguage.labyrinth.operators.SmallerThan;
-import org.emmalanguage.labyrinth.partitioners.Always0;
-import org.emmalanguage.labyrinth.partitioners.Random;
-import org.emmalanguage.labyrinth.util.TupleIntInt;
-import org.emmalanguage.labyrinth.util.Unit;
 import org.apache.flink.api.common.ExecutionConfig;
 import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
@@ -36,21 +23,35 @@ import org.apache.flink.api.common.typeutils.TypeSerializer;
 import org.apache.flink.api.java.typeutils.PojoTypeInfo;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
+import org.emmalanguage.labyrinth.*;
+import org.emmalanguage.labyrinth.operators.*;
+import org.emmalanguage.labyrinth.partitioners.Always0;
+import org.emmalanguage.labyrinth.partitioners.Forward;
+import org.emmalanguage.labyrinth.partitioners.Random;
+import org.emmalanguage.labyrinth.partitioners.RoundRobin;
+import org.emmalanguage.labyrinth.util.TupleIntInt;
+import org.emmalanguage.labyrinth.util.Unit;
 
 import java.util.Arrays;
 
 /**
  * // BB 0
  * i = 1
+ * coll = <200 elements>
  * do {
  *     // BB 1
  *     i = i + 1
+ *     coll.map{x => x + 1}
  * } while (i < 100)
  * // BB 2
  * assert i == 100
  */
 
-public class SimpleCF {
+public class ControlFlowMicrobenchmark {
+
+	private static TypeInformation<ElementOrEvent<Integer>> integerTypeInfo = TypeInformation.of(new TypeHint<ElementOrEvent<Integer>>(){});
+	private static TypeInformation<ElementOrEvent<Boolean>> booleanTypeInfo = TypeInformation.of(new TypeHint<ElementOrEvent<Boolean>>(){});
+	private static TypeInformation<ElementOrEvent<Unit>> unitTypeInfo = TypeInformation.of(new TypeHint<ElementOrEvent<Unit>>(){});
 
 	private static TypeSerializer<Integer> integerSer = TypeInformation.of(Integer.class).createSerializer(new ExecutionConfig());
 	private static TypeSerializer<Boolean> booleanSer = TypeInformation.of(Boolean.class).createSerializer(new ExecutionConfig());
@@ -65,6 +66,7 @@ public class SimpleCF {
 		//env.getConfig().setParallelism(1);
 
 		final int n = Integer.parseInt(args[0]);
+		final int numElements = Integer.parseInt(args[1]);
 
 		PojoTypeInfo.registerCustomSerializer(ElementOrEvent.class, new ElementOrEvent.ElementOrEventSerializerFactory());
 		PojoTypeInfo.registerCustomSerializer(TupleIntInt.class, TupleIntInt.TupleIntIntSerializer.class);
@@ -77,30 +79,51 @@ public class SimpleCF {
 
 		Integer[] input = new Integer[]{1};
 
-		LabySource<Integer> inputBag = new LabySource<>(env.fromCollection(Arrays.asList(input)), 0, TypeInformation.of(new TypeHint<ElementOrEvent<Integer>>(){}));
+		LabySource<Integer> inputBag = new LabySource<>(env.fromCollection(Arrays.asList(input)), 0, integerTypeInfo);
+
+		Integer[] input2 = new Integer[numElements];
+		for (int i=0; i<numElements; i++) {
+			input2[i] = 0;
+		}
+
+		LabySource<Integer> inputBag2 = new LabySource<>(env.fromCollection(Arrays.asList(input2)), 0, integerTypeInfo);
+
+		LabyNode<Integer, Integer> inputBag2Partitioned =
+				new LabyNode<>("inputBag2Partitioned", new IdMap<>(), 0, new RoundRobin<>(env.getParallelism()), integerSer, integerTypeInfo)
+						.addInput(inputBag2, true);
 
 		LabyNode<Integer, Integer> phi =
-				LabyNode.phi("phi", 1, new Random<>(env.getParallelism()), integerSer, TypeInformation.of(new TypeHint<ElementOrEvent<Integer>>(){}))
+				LabyNode.phi("phi", 1, new Random<>(env.getParallelism()), integerSer, integerTypeInfo)
 						.addInput(inputBag, false);
 
+		LabyNode<Integer, Integer> phi2 =
+				LabyNode.phi("phi2", 1, new Random<>(env.getParallelism()), integerSer, integerTypeInfo)
+						.addInput(inputBag2Partitioned, false, true);
+
 		LabyNode<Integer, Integer> inced =
-				new LabyNode<>("inc-map", new IncMap(), 1, new Random<>(env.getParallelism()), integerSer, TypeInformation.of(new TypeHint<ElementOrEvent<Integer>>(){}))
+				new LabyNode<>("inc-map", new IncMap(), 1, new Random<>(env.getParallelism()), integerSer, integerTypeInfo)
+						.addInput(phi, true, false);
+
+		LabyNode<Integer, Integer> inced2 =
+				new LabyNode<>("inc-map2", new IncMap(), 1, new Forward<>(env.getParallelism()), integerSer, integerTypeInfo)
 						.addInput(phi, true, false);
 
 		phi.addInput(inced, false, true);
 
+		phi2.addInput(inced2, false, true);
+
 		LabyNode<Integer, Boolean> smallerThan =
-				new LabyNode<>("smaller-than", new SmallerThan(n), 1, new Always0<>(1), integerSer, TypeInformation.of(new TypeHint<ElementOrEvent<Boolean>>(){}))
+				new LabyNode<>("smaller-than", new SmallerThan(n), 1, new Always0<>(1), integerSer, booleanTypeInfo)
 						.addInput(inced, true, false)
 						.setParallelism(1);
 
 		LabyNode<Boolean, Unit> exitCond =
-				new LabyNode<>("exit-cond", new ConditionNode(1,2), 1, new Always0<>(1), booleanSer, TypeInformation.of(new TypeHint<ElementOrEvent<Unit>>(){}))
+				new LabyNode<>("exit-cond", new ConditionNode(1,2), 1, new Always0<>(1), booleanSer, unitTypeInfo)
 						.addInput(smallerThan, true, false)
 						.setParallelism(1);
 
 		LabyNode<Integer, Unit> assertEquals =
-				new LabyNode<>("Check i == " + n, new AssertEquals<>(n), 2, new Always0<>(1), integerSer, TypeInformation.of(new TypeHint<ElementOrEvent<Unit>>(){}))
+				new LabyNode<>("Check i == " + n, new AssertEquals<>(n), 2, new Always0<>(1), integerSer, unitTypeInfo)
 					.addInput(inced, false, true)
 					.setParallelism(1);
 
